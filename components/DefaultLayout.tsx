610be71172d248952b2a12de3e806e3feeff6418
Irvan Malik <irvanmalik48@gmail.com>
2022-07-19 17:07:17 +0700

    feat: update default layout
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/DefaultLayout.tsx
+++ components/DefaultLayout.tsx
@@ -1,6 +1,6 @@
 /** @jsx h */
 import { h } from "preact";
-import { tw } from "@twind";
+import { apply, tw } from "@twind";
 import Sidebar from "../islands/Sidebar.tsx";
 import Nav from "../islands/Nav.tsx";
 import { css } from "twind/css";
@@ -82,11 +82,8 @@ export default function DefaultLayout(props: {
       <section
         class={tw`sticky top-0 w-full px-4 md:px-6 lg:px-12 xl:px-24 py-4 text-dark-text h-screen overflow-y-auto ${css(
           {
-            "-ms-overflow-style": "none",
-            "scrollbar-width": "none",
-            "&::-webkit-scrollbar": {
-              display: "none",
-            },
+            "&::-webkit-scrollbar": apply`bg-dark-accent-quartertrans w-5`,
+            "&::-webkit-scrollbar-thumb": apply`bg-dark-accent-solid border-transparent border-[7px] border-solid bg-clip-content rounded-xl`,
           }
         )}`}
       >
