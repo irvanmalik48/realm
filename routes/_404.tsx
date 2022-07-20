9f44a19a4c90582dcc586335951d6d274b7b72dd
Irvan Malik <irvanmalik48@gmail.com>
2022-07-20 09:51:36 +0700

    feat: add custom 404 page
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

---
+++ routes/_404.tsx
@@ -0,0 +1,24 @@
+/** @jsx h */
+import { h } from "preact";
+import { UnknownPageProps } from "$fresh/server.ts";
+import DefaultLayout from "../components/DefaultLayout.tsx";
+import { tw } from "../utils/twind.ts";
+
+export default function NotFoundPage({ url }: UnknownPageProps) {
+  return (
+    <DefaultLayout title="Page not found" date={undefined} desc="The page you're looking for is not found." tag={undefined}>
+    <header
+      class={tw`py-24 w-full flex flex-row justify-center items-center gap-5`}
+    >
+      <div class={tw`flex flex-col justify-center items-center`}>
+        <p class={tw`text-dark-text text-center font-bold text-3xl`}>
+          404
+        </p>
+        <p class={tw`w-full text-center text-dark-accent-solid text-lg`}>
+          Page <strong>{url.pathname}</strong> not found.
+        </p>
+      </div>
+    </header>
+  </DefaultLayout>
+    );
+}
