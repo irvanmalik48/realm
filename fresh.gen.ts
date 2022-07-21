fdc5b68114c5e3f181b81dc2504d000cc0099772
Irvan Malik <irvanmalik48@gmail.com>
2022-07-21 14:13:23 +0700

    feat: add quotes API
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,14 +3,14 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import * as $0 from "./routes/_404.tsx";
-import * as $1 from "./routes/api/joke.ts";
+import * as $1 from "./routes/api/quotes.ts";
 import * as $2 from "./routes/index.tsx";
 import * as $3 from "./routes/posts/[slug].tsx";
 
 const manifest = {
   routes: {
     "./routes/_404.tsx": $0,
-    "./routes/api/joke.ts": $1,
+    "./routes/api/quotes.ts": $1,
     "./routes/index.tsx": $2,
     "./routes/posts/[slug].tsx": $3,
   },
---
+++ routes/api/quotes.ts
@@ -0,0 +1,18 @@
+import { HandlerContext } from "$fresh/server.ts";
+
+const quotes = [
+  "Balanced. As all things should be.",
+  "Inner peace is what you should always consider seeking.",
+  "Do what you want and do it well.",
+  "Keep it simple, stupid.",
+  "Hail to the chaos banner!",
+  "Bow before the god of chaos.",
+  "Change is about acceptance, not ignorance towards past.",
+  "We all commit crimes. It's the intention that differs."
+];
+
+export const handler = (_req: Request, _ctx: HandlerContext): Response => {
+  const randomIndex = Math.floor(Math.random() * 10);
+  const body = quotes[randomIndex];
+  return new Response(JSON.parse(body));
+};
