df337dd3777e4e2686daf72945a5d33b8969212a
Irvan Malik <irvanmalik48@gmail.com>
2022-07-21 10:27:56 +0700

    chore: move all icons to one file
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,19 +5,9 @@ import { apply, tw } from "@twind";
 import { css } from "twind/css";
 import { loadContent } from "../utils/load.ts";
 import PostCard from "./PostCard.tsx";
+import { SearchIcon } from "./Icons.tsx";
 
 const posts = await loadContent("posts/");
-
-function SearchIcon() {
-  return (
-    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-      <path
-        fill="currentColor"
-        d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"
-      />
-    </svg>
-  );
-}
 
 export default function Sidebar() {
   const postProps: any[] = [];
