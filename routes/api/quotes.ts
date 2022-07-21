6d456edd69a1c413f6d88530ba999c83c308ad14
Irvan Malik <irvanmalik48@gmail.com>
2022-07-21 23:00:04 +0700

    feat: new shit
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- routes/api/quotes.ts
+++ routes/api/quotes.ts
@@ -6,19 +6,15 @@ const quotes = [
   "Do what you want and do it well.",
   "Keep it simple, stupid.",
   "Hail to the chaos banner!",
-  "Bow before the god of chaos.",
   "Change is about acceptance, not ignorance towards past.",
   "We all commit crimes. It's the intention that differs.",
-  "Stormviper is a cunt and we all know it.",
-  "Only look upon those who are deserved to be.",
-  "Skill issue + ratio + you fell down.",
   "This is why we can't have nice things",
   "PSD is not my favorite file format",
   "I'm not a web developer. I'm a retard."
 ];
 
 export const handler = (_req: Request, _ctx: HandlerContext): Response => {
-  const randomIndex = Math.floor(Math.random() * 14);
+  const randomIndex = Math.floor(Math.random() * 10);
   const body = quotes[randomIndex];
   return new Response(JSON.stringify(body));
 };
