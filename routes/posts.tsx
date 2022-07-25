8fcfa7b5455c945aef824e05c8cf6eddc1dbff81
Irvan Malik <irvanmalik48@gmail.com>
2022-07-25 13:34:06 +0700

    chore: remove totally unnecessary stuff
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- routes/posts.tsx
+++ routes/posts.tsx
@@ -8,22 +8,6 @@ import PostCard from "../components/PostCard.tsx";
 
 const posts = await loadContent("posts/");
 
-const quotes = [
-  "Balanced. As all things should be.",
-  "Inner peace is what you should always consider seeking.",
-  "Do what you want and do it well.",
-  "Keep it simple, stupid.",
-  "Hail to the chaos banner!",
-  "Change is about acceptance, not ignorance towards past.",
-  "We all commit crimes. It's the intention that differs.",
-  "This is why we can't have nice things.",
-  "PSD is not my favorite file format.",
-  "I'm not a web developer. I'm a retard.",
-];
-
-const randomIndex = Math.floor(Math.random() * 10);
-const body = quotes[randomIndex];
-
 export default function Posts() {
   const postProps: any[] = [];
 
