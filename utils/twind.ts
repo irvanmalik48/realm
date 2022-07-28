e68a5ca5f37710f9a3980bcb0f9d439bb5bdc3e4
Irvan Malik <irvanmalik48@gmail.com>
2022-07-28 08:39:02 +0700

    feat: use `important` globally cause why the fuck not
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- utils/twind.ts
+++ utils/twind.ts
@@ -31,6 +31,7 @@ export const config: Configuration = {
   mode: strict,
   darkMode: "class",
   hash: true,
+  important: true,
   preflight: {
     "@import": [
       `url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap')`,
