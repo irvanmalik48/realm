a56cbeebf17e2e6e4d4f253d6bc0a5898edecd5f
Irvan Malik <irvanmalik48@gmail.com>
2022-08-02 13:29:52 +0700

    feat: adjust the 404
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- routes/posts/[slug].tsx
+++ routes/posts/[slug].tsx
@@ -110,13 +110,21 @@ export default function PostPage({ data, ...props }: PageProps<Post | null>) {
           className={tw`py-24 w-full flex flex-row justify-center items-center gap-5`}
         >
           <div className={tw`flex flex-col justify-center items-center`}>
-            <p className={tw`text-dark-text text-center font-bold text-3xl`}>
+            <p
+              className={tw`text-dark-text text-center font-bold text-4xl mb-2 font-heading`}
+            >
               404
             </p>
             <p
               className={tw`w-full text-center text-dark-accent-solid text-lg`}
             >
-              Page <strong>{data?.path?.replace(".md", "")}</strong> not found.
+              Page{" "}
+              <code
+                className={tw`font-mono bg-dark-accent-quartertrans text-sm text-dark-accent-solid font-semibold px-2.5 py-0.5 my-1 rounded-3xl`}
+              >
+                {data?.path?.replace(".md", "")}
+              </code>{" "}
+              not found.
             </p>
           </div>
         </header>
