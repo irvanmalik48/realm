6290949746583f10d10c1575e380662d875764d3
Irvan Malik <irvanmalik48@gmail.com>
2022-08-02 10:41:23 +0700

    feat: disable webkit tap highlight
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/DefaultLayout.tsx
+++ components/DefaultLayout.tsx
@@ -24,6 +24,7 @@ export default function DefaultLayout(props: {
           "linear-gradient(45deg, rgba(0, 0, 0, 0) 50%, #242933 50%, #88C0D010 60%, rgba(0, 0, 0, 0) 50%)",
         "background-size": "2rem 2rem",
         "background-color": "#242933",
+        "-webkit-tap-highlight-color": "transparent",
       })} w-screen gap-0 flex flex-col-reverse md:grid md:grid-cols-tablet lg:grid-cols-root`}
     >
       <Head>
