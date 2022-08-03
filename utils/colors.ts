1134829f22b40bf5c6b947cb0dc9bf1a9f87f6ec
Irvan Malik <irvanmalik48@gmail.com>
2022-08-03 14:07:18 +0700

    feat: separate colors
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

---
+++ utils/colors.ts
@@ -0,0 +1,18 @@
+export const colorScheme = {
+  dark: {
+    nav: "#2E3440",
+    bg: "#242933",
+    side: "#292E39",
+    accent: {
+      solid: "#88C0D0",
+      semitrans: "#88C0D040",
+      quartertrans: "#88C0D020",
+      quitetrans: "#88C0D010",
+      alt: "#7FB2C2",
+    },
+    text: "#D8DEE9",
+    footertext: "#D8DEE9AA",
+  },
+}
+
+export const syntaxHighlighterTheme = "nord";
