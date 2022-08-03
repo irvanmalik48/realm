69ea4fbe229e8ab0d35ce10c8a4f62307f21617c
Irvan Malik <irvanmalik48@gmail.com>
2022-08-03 14:07:29 +0700

    feat: use separated color var
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- utils/twind.ts
+++ utils/twind.ts
@@ -1,5 +1,6 @@
 import { IS_BROWSER } from "$fresh/runtime.ts";
 import { Configuration, setup, strict } from "twind";
+import { colorScheme } from "@utils/colors.ts";
 
 export * from "twind";
 export const config: Configuration = {
@@ -13,18 +14,7 @@ export const config: Configuration = {
         heading: ["Poppins", "sans-serif"],
       },
       colors: {
-        dark: {
-          nav: "#2E3440",
-          bg: "#242933",
-          side: "#292E39",
-          accent: {
-            solid: "#88C0D0",
-            semitrans: "#88C0D040",
-            quartertrans: "#88C0D020",
-          },
-          text: "#D8DEE9",
-          footertext: "#D8DEE9AA",
-        },
+        ...colorScheme,
       },
       gridTemplateColumns: {
         root: "min-content min-content auto",
