c719bac198a0f840cb8fa38acd132f2d719e9f18
Irvan Malik <irvanmalik48@gmail.com>
2022-08-05 23:29:15 +0700

    feat: add everblush color scheme
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- utils/colors.ts
+++ utils/colors.ts
@@ -1,18 +1,38 @@
 export const colorScheme = {
-  dark: {
-    nav: "#2E3440",
-    bg: "#242933",
-    side: "#292E39",
-    accent: {
-      solid: "#88C0D0",
-      semitrans: "#88C0D040",
-      quartertrans: "#88C0D020",
-      quitetrans: "#88C0D010",
-      alt: "#7FB2C2",
+  nord: {
+    dark: {
+      nav: "#2E3440",
+      bg: "#242933",
+      side: "#292E39",
+      accent: {
+        solid: "#88C0D0",
+        semitrans: "#88C0D040",
+        quartertrans: "#88C0D020",
+        quitetrans: "#88C0D010",
+        alt: "#7FB2C2",
+      },
+      text: "#D8DEE9",
+      footertext: "#D8DEE9AA",
     },
-    text: "#D8DEE9",
-    footertext: "#D8DEE9AA",
   },
+  everblush: {
+    dark: {
+      nav: "#282F32",
+      bg: "#141B1E",
+      side: "#232A2D",
+      accent: {
+        solid: "#5DB7CB",
+        semitrans: "#5DB7CB40",
+        quartertrans: "#5DB7CB20",
+        quitetrans: "#5DB7CB10",
+        alt: "#6CBFBF",
+      },
+      text: "#DADADA",
+      footertext: "#DADADAAA",
+    },
+  }
 };
 
-export const syntaxHighlighterTheme = "nord";
+export const currentColorScheme = "everblush";
+
+export const syntaxHighlighterTheme = "everblush";
