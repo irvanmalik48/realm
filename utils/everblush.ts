38c8e866094b339b432230d43f3af7b692c5a66f
Irvan Malik <irvanmalik48@gmail.com>
2022-08-05 23:31:31 +0700

    feat: add syntax highlighting everblush theme
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

---
+++ utils/everblush.ts
@@ -0,0 +1,152 @@
+export const everblush = {
+  "code[class*=\"language-\"]": {
+      "color": "#DADADA",
+      "background": "none",
+      "fontFamily": "\"Fira Code\", Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace",
+      "textAlign": "left",
+      "whiteSpace": "pre",
+      "wordSpacing": "normal",
+      "wordBreak": "normal",
+      "wordWrap": "normal",
+      "lineHeight": "1.5",
+      "MozTabSize": "4",
+      "OTabSize": "4",
+      "tabSize": "4",
+      "WebkitHyphens": "none",
+      "MozHyphens": "none",
+      "msHyphens": "none",
+      "hyphens": "none"
+  },
+  "pre[class*=\"language-\"]": {
+      "color": "#DADADA",
+      "background": "#0D1417",
+      "fontFamily": "\"Fira Code\", Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace",
+      "textAlign": "left",
+      "whiteSpace": "pre",
+      "wordSpacing": "normal",
+      "wordBreak": "normal",
+      "wordWrap": "normal",
+      "lineHeight": "1.5",
+      "MozTabSize": "4",
+      "OTabSize": "4",
+      "tabSize": "4",
+      "WebkitHyphens": "none",
+      "MozHyphens": "none",
+      "msHyphens": "none",
+      "hyphens": "none",
+      "padding": "1em",
+      "margin": ".5em 0",
+      "overflow": "auto",
+      "borderRadius": "0.3em"
+  },
+  ":not(pre) > code[class*=\"language-\"]": {
+      "background": "#0D1417",
+      "padding": ".1em",
+      "borderRadius": ".3em",
+      "whiteSpace": "normal"
+  },
+  "comment": {
+      "color": "#6CBFBF"
+  },
+  "prolog": {
+      "color": "#6CBFBF"
+  },
+  "doctype": {
+      "color": "#6CBFBF"
+  },
+  "cdata": {
+      "color": "#6CBFBF"
+  },
+  "punctuation": {
+      "color": "#67B0E8"
+  },
+  ".namespace": {
+      "Opacity": ".7"
+  },
+  "property": {
+      "color": "#67B0E8"
+  },
+  "tag": {
+      "color": "#67B0E8"
+  },
+  "constant": {
+      "color": "#67B0E8"
+  },
+  "symbol": {
+      "color": "#67B0E8"
+  },
+  "deleted": {
+      "color": "#67B0E8"
+  },
+  "number": {
+      "color": "#C47FD5"
+  },
+  "boolean": {
+      "color": "#67B0E8"
+  },
+  "selector": {
+      "color": "#8CCF7E"
+  },
+  "attr-name": {
+      "color": "#8CCF7E"
+  },
+  "string": {
+      "color": "#8CCF7E"
+  },
+  "char": {
+      "color": "#8CCF7E"
+  },
+  "builtin": {
+      "color": "#8CCF7E"
+  },
+  "inserted": {
+      "color": "#8CCF7E"
+  },
+  "operator": {
+      "color": "#67B0E8"
+  },
+  "entity": {
+      "color": "#67B0E8",
+      "cursor": "help"
+  },
+  "url": {
+      "color": "#67B0E8"
+  },
+  ".language-css .token.string": {
+      "color": "#67B0E8"
+  },
+  ".style .token.string": {
+      "color": "#67B0E8"
+  },
+  "variable": {
+      "color": "#67B0E8"
+  },
+  "atrule": {
+      "color": "#E5C76B"
+  },
+  "attr-value": {
+      "color": "#E5C76B"
+  },
+  "function": {
+      "color": "#E5C76B"
+  },
+  "class-name": {
+      "color": "#E5C76B"
+  },
+  "keyword": {
+      "color": "#67B0E8"
+  },
+  "regex": {
+      "color": "#E57474"
+  },
+  "important": {
+      "color": "#E57474",
+      "fontWeight": "bold"
+  },
+  "bold": {
+      "fontWeight": "bold"
+  },
+  "italic": {
+      "fontStyle": "italic"
+  }
+}
