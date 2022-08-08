4c2e851f6cce961d865556b4bc6869046e22bb4c
Irvan Malik <irvanmalik48@gmail.com>
2022-08-08 14:54:39 +0700

    chore: separate preblock
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

---
+++ components/PreBlock.tsx
@@ -0,0 +1,18 @@
+/** @jsx h */
+import { h } from "preact";
+import { Any } from "any";
+import CodeBlock from "@components/CodeBlock.tsx";
+
+export default function PreBlock(props: {
+  children: Any;
+  rest: h.JSX.IntrinsicAttributes & h.JSX.HTMLAttributes<HTMLPreElement>;
+}) {
+  if ("type" in props.children && props.children["type"] === "code") {
+    return CodeBlock(props.children.props);
+  }
+  return (
+    <pre {...props.rest} className="notranslate" translate="no">
+      {props.children}
+    </pre>
+  );
+}
