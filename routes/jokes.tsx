2f395d90889d14d658e77b17034866d5b1c7c3bd
Irvan Malik <irvanmalik48@gmail.com>
2022-08-09 19:02:06 +0700

    feat: jokes update with img me dumb sir
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- routes/jokes.tsx
+++ routes/jokes.tsx
@@ -1,17 +1,34 @@
 /** @jsx h */
-import { h } from "preact";
+import { h, Fragment } from "preact";
 import { Handlers, PageProps } from "$fresh/server.ts";
 import DefaultLayout from "@components/DefaultLayout.tsx";
 import { tw } from "@utils/twind.ts";
 import { css } from "twind/css";
-import { JokesType } from "@/types.d.tsx";
+import { JokesImage, JokesText, JokesType } from "@/types.d.tsx";
 
-export const handler: Handlers<JokesType | null> = {
-  async GET(_req, ctx) {
-    const res = await fetch("https://candaan-api.vercel.app/api/text/random");
-    const body: JokesType = await res.json();
+export const handler: Handlers<JokesType> = {
+  async GET(req, ctx) {
+    const includeParams = new URL(req.url).searchParams.get("type");
+    const res: {
+      text: JokesText,
+      img: JokesImage
+    } = {
+      text: await (await fetch("https://candaan-api.vercel.app/api/text/random")).json(),
+      img: await (await fetch("https://candaan-api.vercel.app/api/image/random")).json(),
+    };
+    const cases = {
+      _default: res,
+      text: {
+        text: res.text
+      },
+      image: {
+        img: res.img
+      },
+    }
+    
+    const body: JokesType = includeParams !== null && includeParams !== "all" ? cases[includeParams as keyof typeof cases] : cases["_default"];
 
-    if (res.status === 404) {
+    if (res.text.status === 404 || res.img.status === 404) {
       return ctx.render(null);
     }
 
@@ -19,7 +36,7 @@ export const handler: Handlers<JokesType | null> = {
   },
 };
 
-export default function Jokes(props: PageProps<JokesType>) {
+export default function Jokes(props: PageProps<{ text?: JokesText, img?: JokesImage }>) {
   return (
     <DefaultLayout
       title="Jokes Indonesia"
@@ -28,23 +45,48 @@ export default function Jokes(props: PageProps<JokesType>) {
       <header
         className={tw`py-24 w-full flex flex-row justify-center items-center gap-5`}
       >
-        <div className={tw`flex flex-col justify-center items-center`}>
+        <div className={tw`w-full flex flex-col justify-center items-center`}>
           <p
-            className={tw`text-dark-text text-center font-bold text-4xl mb-2 font-heading`}
+            className={tw`text-dark-text text-center font-bold text-4xl mb-10 font-heading`}
           >
             Generated Joke:
           </p>
-          <p
-            className={tw`w-full text-center text-xl px-5 py-3 bg-dark-accent-quartertrans rounded-xl text-dark-text box-border ${css(
+          <div
+            className={tw`w-full text-center flex flex-col gap-5 text-xl p-5 pt-3 bg-dark-accent-quartertrans rounded-xl text-dark-text box-border ${css(
               {
                 "backdrop-filter": "blur(.5rem)",
               }
             )}`}
           >
-            {props.data.data}
-          </p>
+            {props.data.text ? <TextJoke data={props.data.text} /> : ""}
+            {props.data.img ? <ImageJoke data={props.data.img} /> : ""}
+          </div>
         </div>
       </header>
     </DefaultLayout>
   );
+}
+
+function TextJoke(props: { data: JokesText | undefined }) {
+  return <div>
+    <p
+      className={tw`text-dark-text text-left font-bold text-xl mb-2 font-heading`}
+    >
+      Text:
+    </p>
+    <p className={tw`text-dark-text text-center px-5 py-3 text-lg bg-dark-accent-semitrans rounded-xl`}>
+      {props?.data?.data}
+    </p>
+  </div>;
+}
+
+function ImageJoke(props: { data: JokesImage | undefined }) {
+  return <div>
+    <p
+      className={tw`text-dark-text text-left font-bold text-xl mb-2 font-heading`}
+    >
+      Image:
+    </p>
+    <img className={tw`w-full h-[500px] object-contain bg-dark-superdark transition-all duration-200 ease-linear ring ring-transparent hover:ring-dark-accent-solid rounded-xl`} src={props?.data?.data.url} alt="joke" />
+  </div>;
 }
