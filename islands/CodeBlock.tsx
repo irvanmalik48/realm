da47b9691ab037522657e4f31b7adf94066576cf
Irvan Malik <irvanmalik48@gmail.com>
2022-08-14 00:41:17 +0700

    chore: prettier
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- islands/CodeBlock.tsx
+++ islands/CodeBlock.tsx
@@ -53,22 +53,26 @@ export default function CodeBlock(props: {
       className={tw`text-dark-text font-mono bg-dark-bg text-sm overflow-x-auto my-3 rounded-xl overflow-clip`}
     >
       <div className={tw`px-5 py-2 bg-dark-superdark`}>
-      <p className={tw`w-full flex flex-row justify-between items-center`}>
-        {fileName !== "none" && (
+        <p className={tw`w-full flex flex-row justify-between items-center`}>
+          {fileName !== "none" && (
             <span
               className={tw`font-mono bg-dark-accent-semitrans w-[fit-content] text-sm text-dark-accent-solid font-semibold px-4 py-0.5 my-1 rounded-3xl`}
             >
               {fileName}
             </span>
-        )}
-        <button
-          className={tw`font-mono inline-block bg-dark-nav hover:bg-dark-accent-semitrans active:bg-dark-accent-solid focus:outline-none focus:bg-dark-accent-solid ring ring-transparent hover:ring-dark-accent-solid w-[fit-content] text-sm text-dark-text active:text-dark-superdark focus:text-dark-superdark hover:text-dark-accent-solid transition-all duration-200 ease-linear font-semibold px-4 py-0.5 my-1 rounded-3xl`}
-          onClick={handleClick}
-          disabled={!IS_BROWSER}
-        >
-          {copied ? <CheckIcon className={tw`w-[1rem]`} /> : <CopyIcon className={tw`w-[1rem]`} />}
-        </button>
-      </p>
+          )}
+          <button
+            className={tw`font-mono inline-block bg-dark-nav hover:bg-dark-accent-semitrans active:bg-dark-accent-solid focus:outline-none focus:bg-dark-accent-solid ring ring-transparent hover:ring-dark-accent-solid w-[fit-content] text-sm text-dark-text active:text-dark-superdark focus:text-dark-superdark hover:text-dark-accent-solid transition-all duration-200 ease-linear font-semibold px-4 py-0.5 my-1 rounded-3xl`}
+            onClick={handleClick}
+            disabled={!IS_BROWSER}
+          >
+            {copied ? (
+              <CheckIcon className={tw`w-[1rem]`} />
+            ) : (
+              <CopyIcon className={tw`w-[1rem]`} />
+            )}
+          </button>
+        </p>
       </div>
       <Prism
         language={lang}
