3cb042406de75de8330a4cccf69469976accefaf
Irvan Malik <irvanmalik48@gmail.com>
2022-08-14 00:40:17 +0700

    feat: add copy to clipboard (probably)
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/PreBlock.tsx
+++ components/PreBlock.tsx
@@ -1,7 +1,7 @@
 /** @jsx h */
 import { h } from "preact";
 import { Any } from "any";
-import CodeBlock from "@components/CodeBlock.tsx";
+import CodeBlock from "@islands/CodeBlock.tsx";
 
 export default function PreBlock(props: {
   children: Any;
