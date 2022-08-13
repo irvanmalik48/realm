5a91cb9ea13c51568c21cb23b1d4137b3882db3a
Irvan Malik <irvanmalik48@gmail.com>
2022-08-13 22:29:39 +0700

    feat: i probably can do it for a while
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -11,7 +11,9 @@ import * as $5 from "./routes/jokes.tsx";
 import * as $6 from "./routes/posts.tsx";
 import * as $7 from "./routes/posts/[slug].tsx";
 import * as $8 from "./routes/sitemap.xml.ts";
-import * as $$0 from "./islands/WaterDrop.tsx";
+import * as $$0 from "./islands/Nav.tsx";
+import * as $$1 from "./islands/Sidebar.tsx";
+import * as $$2 from "./islands/WaterDrop.tsx";
 
 const manifest = {
   routes: {
@@ -26,7 +28,9 @@ const manifest = {
     "./routes/sitemap.xml.ts": $8,
   },
   islands: {
-    "./islands/WaterDrop.tsx": $$0,
+    "./islands/Nav.tsx": $$0,
+    "./islands/Sidebar.tsx": $$1,
+    "./islands/WaterDrop.tsx": $$2,
   },
   baseUrl: import.meta.url,
 };
