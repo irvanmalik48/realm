f86bd65388ab85fa8e1519fb92275de5aa7439b7
Irvan Malik <irvanmalik48@gmail.com>
2022-08-14 10:16:31 +0700

    feat: simplify
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- islands/CopyButton.tsx
+++ islands/CopyButton.tsx
@@ -34,7 +34,7 @@ export default function CopyButton(props: { text: string }) {
 
   return (
     <button
-      className={tw`font-mono inline-block bg-dark-nav hover:bg-dark-accent-semitrans active:bg-dark-accent-solid focus:outline-none focus:bg-dark-accent-solid ring ring-transparent hover:ring-dark-accent-solid w-[fit-content] text-sm text-dark-text active:text-dark-superdark focus:text-dark-superdark hover:text-dark-accent-solid transition-all duration-200 ease-linear font-semibold px-4 py-0.5 my-1 rounded-3xl`}
+      className={tw`font-mono inline-block bg-dark-nav hover:bg-dark-accent-semitrans active:bg-dark-accent-solid focus:outline-none ring ring-transparent focus-visible:ring-dark-accent-solid hover:ring-dark-accent-solid w-[fit-content] text-sm text-dark-text active:text-dark-superdark hover:text-dark-accent-solid transition-all duration-200 ease-linear font-semibold px-4 py-0.5 my-1 rounded-3xl`}
       onClick={async (e) => {
         e.preventDefault();
         await handleClick();
