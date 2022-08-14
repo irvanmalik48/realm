91a45d9bf3f1e4bafc2ae511845f6fa5ed36c48b
Irvan Malik <irvanmalik48@gmail.com>
2022-08-14 09:10:02 +0700

    chore: update imports
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/PreBlock.tsx
+++ components/PreBlock.tsx
@@ -1,7 +1,7 @@
 /** @jsx h */
 import { h } from "preact";
 import { Any } from "any";
-import CodeBlock from "@islands/CodeBlock.tsx";
+import CodeBlock from "@components/CodeBlock.tsx";
 
 export default function PreBlock(props: {
   children: Any;
