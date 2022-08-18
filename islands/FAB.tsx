ef2e481810d90e3b76f6591f35d41353eb6e8c23
Irvan Malik <irvanmalik48@gmail.com>
2022-08-18 19:52:32 +0700

    feat: use `cursor-default` when not visible
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- islands/FAB.tsx
+++ islands/FAB.tsx
@@ -30,7 +30,7 @@ export default function FAB() {
   return (
     <button
       className={tw`fixed ${
-        isVisible ? "opacity-100" : "opacity-0"
+        isVisible ? "opacity-100" : "opacity-0 cursor-default"
       } bottom-[7rem] right-[1rem] md:bottom-[3rem] md:right-[5rem] text-dark-text bg-dark-nav ring ring-dark-accent-semitrans shadow-lg text-dark-accent-solid w-[fit-content] p-4 rounded-full hover:bg-dark-accent-solid hover:ring-dark-accent-solid hover:shadow-xl hover:text-dark-nav transition-all duration-300`}
       aria-label="Back to top"
       onClick={(e) => {
