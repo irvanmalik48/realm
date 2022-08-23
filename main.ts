892d3b0600f18f8f9521149291593cbe6f879c9b
Irvan Malik <irvanmalik48@gmail.com>
2022-08-24 00:14:28 +0700

    feat(main.ts): now `.env` files work properly
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- main.ts
+++ main.ts
@@ -9,5 +9,12 @@ import manifest from "@/fresh.gen.ts";
 
 import twind from "@utils/plugin-twind/mod.ts";
 import { config } from "@utils/twind.ts";
+import { IS_BROWSER } from "$fresh/runtime.ts";
+
+import { loadEnv } from "@utils/load.ts";
+
+if (!IS_BROWSER) {
+  loadEnv();
+}
 
 await start(manifest, { plugins: [twind(config)] });
