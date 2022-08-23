1b0c060145690a1bd51e320980069b1ac911a7ba
Irvan Malik <irvanmalik48@gmail.com>
2022-08-24 00:21:17 +0700

    fix(main.ts): broken build
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- main.ts
+++ main.ts
@@ -9,11 +9,10 @@ import manifest from "@/fresh.gen.ts";
 
 import twind from "@utils/plugin-twind/mod.ts";
 import { config } from "@utils/twind.ts";
-import { IS_BROWSER } from "$fresh/runtime.ts";
 
 import { loadEnv } from "@utils/load.ts";
 
-if (!IS_BROWSER) {
+if (!Deno.env.get("THIS_ENV") && Deno.env.get("THIS_ENV") !== "production") {
   loadEnv();
 }
 
