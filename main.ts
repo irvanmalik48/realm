1175b70ae84383b9fb6c94055866094db7f7937e
Irvan Malik <irvanmalik48@gmail.com>
2022-08-24 18:39:36 +0700

    refactor(env): use deno way
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- main.ts
+++ main.ts
@@ -7,13 +7,9 @@
 import { start } from "$fresh/server.ts";
 import manifest from "@/fresh.gen.ts";
 
+import "$std/dotenv/load.ts";
+
 import twind from "@utils/plugin-twind/mod.ts";
 import { config } from "@utils/twind.ts";
-
-import { loadEnv } from "@utils/load.ts";
-
-if (!Deno.env.get("THIS_ENV") && Deno.env.get("THIS_ENV") !== "production") {
-  loadEnv();
-}
 
 await start(manifest, { plugins: [twind(config)] });
