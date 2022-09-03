ccf59d6c957cf51afcf3e526ead0f178645414fd
Irvan Malik <irvanmalik48@gmail.com>
2022-09-04 00:55:45 +0700

    feat(components/icons): add `CopyCheckIcon`
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -163,6 +163,33 @@ export function CheckIcon(props: { className?: string }) {
   );
 }
 
+export function CopyCheckIcon(
+  props: {
+    className?: string;
+    pathCopyClassName?: string;
+    pathCheckClassName?: string;
+  },
+) {
+  return (
+    <svg
+      className={props.className}
+      style="width:24px;height:24px"
+      viewBox="0 0 24 24"
+    >
+      <path
+        className={props.pathCopyClassName}
+        fill="currentColor"
+        d="M19,21H8V7H19M19,5H8A2,2 0 0,0 6,7V21A2,2 0 0,0 8,23H19A2,2 0 0,0 21,21V7A2,2 0 0,0 19,5M16,1H4A2,2 0 0,0 2,3V17H4V3H16V1Z"
+      />
+      <path
+        className={props.pathCheckClassName}
+        fill="currentColor"
+        d="M23,12L20.56,9.22L20.9,5.54L17.29,4.72L15.4,1.54L12,3L8.6,1.54L6.71,4.72L3.1,5.53L3.44,9.21L1,12L3.44,14.78L3.1,18.47L6.71,19.29L8.6,22.47L12,21L15.4,22.46L17.29,19.28L20.9,18.46L20.56,14.78L23,12M10,17L6,13L7.41,11.59L10,14.17L16.59,7.58L18,9L10,17Z"
+      />
+    </svg>
+  );
+}
+
 export function ChevronUp() {
   return (
     <svg style="width:24px;height:24px" viewBox="0 0 24 24">
