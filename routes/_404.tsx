8c8cce70db217c2e960f7d958594068e8a6465d9
Irvan Malik <irvanmalik48@gmail.com>
2022-09-04 10:30:37 +0700

    chore(routes): use automatic jsx imports
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- routes/_404.tsx
+++ routes/_404.tsx
@@ -1,5 +1,3 @@
-/** @jsx h */
-import { h } from "preact";
 import { UnknownPageProps } from "$fresh/server.ts";
 import NotFoundPageFound from "@/routes/blooper.tsx";
 import DefaultLayout from "@components/DefaultLayout.tsx";
