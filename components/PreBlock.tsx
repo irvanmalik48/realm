6a603e0e2b7f4acc7d162da7928cc8495e1eb4b4
Irvan Malik <irvanmalik48@gmail.com>
2022-09-04 10:29:39 +0700

    chore(components): use automatic jsx imports
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/PreBlock.tsx
+++ components/PreBlock.tsx
@@ -1,16 +1,16 @@
-/** @jsx h */
 import { h } from "preact";
 import { Any } from "any";
 import CodeBlock from "@components/CodeBlock.tsx";
 
-export default function PreBlock(props: {
-  children: Any;
-  rest: h.JSX.IntrinsicAttributes & h.JSX.HTMLAttributes<HTMLPreElement>;
-}) {
-  if ("type" in props.children && props.children["type"] === "code") {
+export default function PreBlock(
+  props: h.JSX.IntrinsicAttributes & h.JSX.HTMLAttributes<HTMLPreElement> & {
+    children: Any;
+  },
+) {
+  if (props.children && props.children["type"] === "code") {
     return CodeBlock(props.children.props);
   }
-  return <pre {...props.rest} className="notranslate" translate="no">
+  return <pre {...props} className="notranslate" translate="no">
       {props.children}
   </pre>;
 }
