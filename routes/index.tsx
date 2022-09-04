84a0fa442a2a9cba783c787a80f61f53fd562740
Irvan Malik <irvanmalik48@gmail.com>
2022-09-04 11:11:48 +0700

    refactor(routes/index): cleanup
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- routes/index.tsx
+++ routes/index.tsx
@@ -3,106 +3,11 @@ import { Post } from "@/types.d.tsx";
 import DefaultLayout from "@components/DefaultLayout.tsx";
 import PostCard from "@components/PostCard.tsx";
 import { loadContent, timeToRead } from "@utils/load.ts";
-import { colorScheme, currentColorScheme } from "@utils/colors.ts";
 import { quotes } from "@utils/quotes.ts";
 import { css, tw } from "@utils/twind.ts";
+import { ChaosLogo } from "@components/ChaosLogo.tsx";
 
 const posts = await loadContent("posts/");
-
-function ChaosLogo(props: { class: string }) {
-  return (
-    <svg
-      width="500"
-      height="500"
-      viewBox="0 0 500 500"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      className={props.class}
-    >
-      <circle
-        cx="250"
-        cy="250"
-        r="194"
-        className={tw`group-hover:stroke-current group-hover:text-transparent transition-all duration-1000 ease-in-out`}
-        stroke={colorScheme[currentColorScheme].dark.nav}
-        stroke-width="12"
-      />
-      <rect
-        x="243"
-        y="100"
-        width="15"
-        height="300"
-        fill={colorScheme[currentColorScheme].dark.accent.solid}
-      />
-      <path
-        d="M250 70L275.981 115H224.019L250 70Z"
-        fill={colorScheme[currentColorScheme].dark.accent.solid}
-      />
-      <path
-        d="M250 430L224.019 385H275.981L250 430Z"
-        fill={colorScheme[currentColorScheme].dark.accent.solid}
-      />
-      <rect
-        x="100"
-        y="257"
-        width="15"
-        height="300"
-        transform="rotate(-90 100 257)"
-        fill={colorScheme[currentColorScheme].dark.accent.solid}
-      />
-      <path
-        d="M70 250L115 224.019V275.981L70 250Z"
-        fill={colorScheme[currentColorScheme].dark.accent.solid}
-      />
-      <path
-        d="M430 250L385 275.981V224.019L430 250Z"
-        fill={colorScheme[currentColorScheme].dark.accent.solid}
-      />
-      <rect
-        x="139.477"
-        y="148.95"
-        width="15"
-        height="300"
-        transform="rotate(-45 139.477 148.95)"
-        fill={colorScheme[currentColorScheme].dark.accent.solid}
-      />
-      <path
-        d="M123.213 122.787L173.404 136.235L136.662 172.978L123.213 122.787Z"
-        fill={colorScheme[currentColorScheme].dark.accent.solid}
-      />
-      <path
-        d="M377.772 377.345L327.581 363.897L364.323 327.154L377.772 377.345Z"
-        fill={colorScheme[currentColorScheme].dark.accent.solid}
-      />
-      <rect
-        x="148.95"
-        y="361.523"
-        width="15"
-        height="300"
-        transform="rotate(-135 148.95 361.523)"
-        fill={colorScheme[currentColorScheme].dark.accent.solid}
-      />
-      <path
-        d="M122.787 377.787L136.235 327.596L172.978 364.338L122.787 377.787Z"
-        fill={colorScheme[currentColorScheme].dark.accent.solid}
-      />
-      <path
-        d="M377.345 123.228L363.897 173.419L327.154 136.677L377.345 123.228Z"
-        fill={colorScheme[currentColorScheme].dark.accent.solid}
-      />
-      <rect
-        x="151.393"
-        y="250"
-        width="140"
-        height="140"
-        transform="rotate(-45 151.393 250)"
-        className={tw`group-hover:stroke-current group-hover:text-transparent transition-all duration-1000 ease-in-out`}
-        stroke={colorScheme[currentColorScheme].dark.nav}
-        stroke-width="15"
-      />
-    </svg>
-  );
-}
 
 export const handler: Handlers<{
   quote: string;
@@ -145,7 +50,7 @@ export default function Home(
           aria-label="Symbol of Chaos - Wikipedia"
         >
           <ChaosLogo
-            class={tw`bg-dark-side group rounded-full w-[200px] md:w-[100px] h-[fit-content] hover:translate-y-[-20px] md:hover:translate-y-[0] md:hover:translate-x-[-20px] hover:rotate-[360deg] hover:scale-[1.2] hover:transform-gpu hover:(ring ring-dark-accent-solid) transition-all duration-1000 ease-in-out`}
+            className={tw`bg-dark-side group rounded-full w-[200px] md:w-[100px] h-[fit-content] hover:translate-y-[-20px] md:hover:translate-y-[0] md:hover:translate-x-[-20px] hover:rotate-[360deg] hover:scale-[1.2] hover:transform-gpu hover:(ring ring-dark-accent-solid) transition-all duration-1000 ease-in-out`}
           />
         </a>
         <div className={tw`flex flex-col justify-start items-center`}>
