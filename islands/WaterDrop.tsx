b6e2df0da27380d230491d2cde13fd32300bbbf8
Irvan Malik <irvanmalik48@gmail.com>
2022-09-04 10:30:30 +0700

    chore(islands): use automatic jsx imports
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- islands/WaterDrop.tsx
+++ islands/WaterDrop.tsx
@@ -1,8 +1,6 @@
 //  SPDX-FileCopyrightText: 2021 Luca Casonato
 //  SPDX-License-Identifier: MIT
 
-/** @jsx h */
-import { h } from "preact";
 import { useEffect, useRef, useState } from "preact/hooks";
 import { Spring } from "@/types.d.tsx";
 import WaveTank from "@components/WaveTank.tsx";
