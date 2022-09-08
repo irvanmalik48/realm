66914eb91ad24e343f4c6ce98c596254737757c3
Irvan Malik <irvanmalik48@gmail.com>
2022-09-08 23:49:47 +0700

    chore(islands/counterlogic): use signals
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- islands/CounterLogic.tsx
+++ islands/CounterLogic.tsx
@@ -1,15 +1,20 @@
-import { useState } from "preact/hooks";
+import { signal } from "signals";
 import { tw } from "@utils/twind.ts";
 
 export default function CounterLogic(props: { start: number }) {
-  let [count, setCount] = useState(props.start);
+  const count = signal(props.start);
+
+  function setCount(val: number) {
+    count.value = val;
+  }
+
   return (
     <>
       <button
         className={tw`bg-dark-accent-semitrans text-dark-accent-solid p-3 rounded-xl w-[50px] h-[50px] text-center transition-all duration-200 ease-out hover:bg-dark-accent-solid hover:rounded-3xl hover:text-dark-nav`}
         onClick={(e) => {
           e.preventDefault();
-          setCount(--count);
+          setCount(--count.value);
         }}
       >
         -1
@@ -19,7 +24,7 @@ export default function CounterLogic(props: { start: number }) {
         className={tw`bg-dark-accent-semitrans text-dark-accent-solid p-3 rounded-xl w-[50px] h-[50px] text-center transition-all duration-200 ease-out hover:bg-dark-accent-solid hover:rounded-3xl hover:text-dark-nav`}
         onClick={(e) => {
           e.preventDefault();
-          setCount(++count);
+          setCount(++count.value);
         }}
       >
         +1
