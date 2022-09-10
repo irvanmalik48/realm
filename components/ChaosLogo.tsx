392a1342dab14d5723d9f31f3cc3477465e77b43
Irvan Malik <irvanmalik48@gmail.com>
2022-09-10 12:04:47 +0700

    feat(components/chaoslogo): light mode
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/ChaosLogo.tsx
+++ components/ChaosLogo.tsx
@@ -15,8 +15,7 @@ export function ChaosLogo(props: { className: string }) {
         cx="250"
         cy="250"
         r="194"
-        className={tw`group-hover:stroke-current group-hover:text-transparent transition-all duration-1000 ease-in-out`}
-        stroke={colorScheme[currentColorScheme].dark.nav}
+        className={tw`stroke-current text-light-nav dark:text-dark-nav group-hover:text-transparent transition-all duration-1000 ease-in-out`}
         stroke-width="12"
       />
       <rect
@@ -24,15 +23,15 @@ export function ChaosLogo(props: { className: string }) {
         y="100"
         width="15"
         height="300"
-        fill={colorScheme[currentColorScheme].dark.accent.solid}
+        className={tw`fill-current text-light-accent-solid dark:text-dark-accent-solid`}
       />
       <path
         d="M250 70L275.981 115H224.019L250 70Z"
-        fill={colorScheme[currentColorScheme].dark.accent.solid}
+        className={tw`fill-current text-light-accent-solid dark:text-dark-accent-solid`}
       />
       <path
         d="M250 430L224.019 385H275.981L250 430Z"
-        fill={colorScheme[currentColorScheme].dark.accent.solid}
+        className={tw`fill-current text-light-accent-solid dark:text-dark-accent-solid`}
       />
       <rect
         x="100"
@@ -40,15 +39,15 @@ export function ChaosLogo(props: { className: string }) {
         width="15"
         height="300"
         transform="rotate(-90 100 257)"
-        fill={colorScheme[currentColorScheme].dark.accent.solid}
+        className={tw`fill-current text-light-accent-solid dark:text-dark-accent-solid`}
       />
       <path
         d="M70 250L115 224.019V275.981L70 250Z"
-        fill={colorScheme[currentColorScheme].dark.accent.solid}
+        className={tw`fill-current text-light-accent-solid dark:text-dark-accent-solid`}
       />
       <path
         d="M430 250L385 275.981V224.019L430 250Z"
-        fill={colorScheme[currentColorScheme].dark.accent.solid}
+        className={tw`fill-current text-light-accent-solid dark:text-dark-accent-solid`}
       />
       <rect
         x="139.477"
@@ -56,15 +55,15 @@ export function ChaosLogo(props: { className: string }) {
         width="15"
         height="300"
         transform="rotate(-45 139.477 148.95)"
-        fill={colorScheme[currentColorScheme].dark.accent.solid}
+        className={tw`fill-current text-light-accent-solid dark:text-dark-accent-solid`}
       />
       <path
         d="M123.213 122.787L173.404 136.235L136.662 172.978L123.213 122.787Z"
-        fill={colorScheme[currentColorScheme].dark.accent.solid}
+        className={tw`fill-current text-light-accent-solid dark:text-dark-accent-solid`}
       />
       <path
         d="M377.772 377.345L327.581 363.897L364.323 327.154L377.772 377.345Z"
-        fill={colorScheme[currentColorScheme].dark.accent.solid}
+        className={tw`fill-current text-light-accent-solid dark:text-dark-accent-solid`}
       />
       <rect
         x="148.95"
@@ -72,15 +71,15 @@ export function ChaosLogo(props: { className: string }) {
         width="15"
         height="300"
         transform="rotate(-135 148.95 361.523)"
-        fill={colorScheme[currentColorScheme].dark.accent.solid}
+        className={tw`fill-current text-light-accent-solid dark:text-dark-accent-solid`}
       />
       <path
         d="M122.787 377.787L136.235 327.596L172.978 364.338L122.787 377.787Z"
-        fill={colorScheme[currentColorScheme].dark.accent.solid}
+        className={tw`fill-current text-light-accent-solid dark:text-dark-accent-solid`}
       />
       <path
         d="M377.345 123.228L363.897 173.419L327.154 136.677L377.345 123.228Z"
-        fill={colorScheme[currentColorScheme].dark.accent.solid}
+        className={tw`fill-current text-light-accent-solid dark:text-dark-accent-solid`}
       />
       <rect
         x="151.393"
@@ -88,8 +87,7 @@ export function ChaosLogo(props: { className: string }) {
         width="140"
         height="140"
         transform="rotate(-45 151.393 250)"
-        className={tw`group-hover:stroke-current group-hover:text-transparent transition-all duration-1000 ease-in-out`}
-        stroke={colorScheme[currentColorScheme].dark.nav}
+        className={tw`stroke-current text-light-nav dark:text-dark-nav group-hover:text-transparent transition-all duration-1000 ease-in-out`}
         stroke-width="15"
       />
     </svg>
