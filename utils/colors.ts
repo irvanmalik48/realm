3d2c78a3df8056b2243f48adf746e8ceff06a875
Irvan Malik <irvanmalik48@gmail.com>
2022-09-10 12:11:42 +0700

    feat(colors): confusingly switch dark to light and add light colors in dark vars lmao
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- utils/colors.ts
+++ utils/colors.ts
@@ -1,6 +1,22 @@
 export const colorScheme = {
   nord: {
     dark: {
+      nav: "#F8F9FB",
+      navglass: "#F8F9FBAA",
+      bg: "#E5E9F0",
+      superdark: "#D8DEE9",
+      side: "#ECEFF4",
+      accent: {
+        solid: "#5E81AC",
+        semitrans: "#5E81AC40",
+        quartertrans: "#5E81AC20",
+        quitetrans: "#5E81AC10",
+        alt: "#5E81AC",
+      },
+      text: "#4C566A",
+      footertext: "#4C566AAA",
+    },
+    light: {
       nav: "#2E3440",
       navglass: "#2E3440AA",
       bg: "#242933",
@@ -19,6 +35,22 @@ export const colorScheme = {
   },
   everblush: {
     dark: {
+      nav: "#EAEBED",
+      navglass: "#EAEBEDAA",
+      bg: "#E7E8E8",
+      superdark: "#DFE0E0",
+      side: "#E8E9EA",
+      accent: {
+        solid: "#3559A6",
+        semitrans: "#3559A630",
+        quartertrans: "#3559A614",
+        quitetrans: "#3559A60E",
+        alt: "#3559A6",
+      },
+      text: "#212C30",
+      footertext: "#212C30AA",
+    },
+    light: {
       nav: "#212C30",
       navglass: "#212C30AA",
       bg: "#141B1E",
