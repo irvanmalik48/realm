318031e14882c851ddbefdeb9e24c397714ff039
Irvan Malik <irvanmalik48@gmail.com>
2022-09-10 12:08:11 +0700

    feat(islands/counterlogic): light mode
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- islands/CounterLogic.tsx
+++ islands/CounterLogic.tsx
@@ -11,7 +11,7 @@ export default function CounterLogic(props: { start: number }) {
   return (
     <>
       <button
-        className={tw`bg-dark-accent-semitrans text-dark-accent-solid p-3 rounded-xl w-[50px] h-[50px] text-center transition-all duration-200 ease-out hover:bg-dark-accent-solid hover:rounded-3xl hover:text-dark-nav`}
+        className={tw`bg-light-accent-semitrans dark:bg-dark-accent-semitrans text-light-accent-solid dark:text-dark-accent-solid p-3 rounded-xl w-[50px] h-[50px] text-center transition-all duration-200 ease-out hover:bg-light-accent-solid hover:dark:bg-dark-accent-solid hover:rounded-3xl hover:text-light-nav hover:dark:text-dark-nav`}
         onClick={(e) => {
           e.preventDefault();
           setCount(--count.value);
@@ -19,9 +19,11 @@ export default function CounterLogic(props: { start: number }) {
       >
         -1
       </button>
-      <p className={tw`font-bold text-2xl`}>{count}</p>
+      <p className={tw`text-light-text dark:text-dark-text font-bold text-2xl`}>
+        {count}
+      </p>
       <button
-        className={tw`bg-dark-accent-semitrans text-dark-accent-solid p-3 rounded-xl w-[50px] h-[50px] text-center transition-all duration-200 ease-out hover:bg-dark-accent-solid hover:rounded-3xl hover:text-dark-nav`}
+        className={tw`bg-light-accent-semitrans dark:bg-dark-accent-semitrans text-light-accent-solid dark:text-dark-accent-solid p-3 rounded-xl w-[50px] h-[50px] text-center transition-all duration-200 ease-out hover:bg-light-accent-solid hover:dark:bg-dark-accent-solid hover:rounded-3xl hover:text-light-nav hover:dark:text-dark-nav`}
         onClick={(e) => {
           e.preventDefault();
           setCount(++count.value);
