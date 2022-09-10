90df739bb67aefd3f783f62ecfa632ab982731c7
Irvan Malik <irvanmalik48@gmail.com>
2022-09-10 12:06:10 +0700

    feat(components/donate): light mode
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/Donate.tsx
+++ components/Donate.tsx
@@ -14,7 +14,7 @@ function CoffeeIcon() {
 export default function DonateCard() {
   return (
     <div
-      className={tw`flex flex-row justify-between items-center w-full px-5 py-3 bg-dark-accent-quartertrans rounded-xl text-dark-text box-border mb-10 ${
+      className={tw`flex flex-row justify-between items-center w-full px-5 py-3 bg-light-accent-quartertrans dark:bg-dark-accent-quartertrans rounded-xl text-light-text dark:text-dark-text box-border mb-10 ${
         css(
           {
             "-webkit-backdrop-filter": "blur(.5rem)",
@@ -25,35 +25,37 @@ export default function DonateCard() {
     >
       <div>
         <div>
-          <p className={tw`text-dark-accent-solid font-semibold font-heading`}>
+          <p
+            className={tw`text-light-accent-solid dark:text-dark-accent-solid font-semibold font-heading`}
+          >
             So, a cup of coffee for me?
           </p>
-          <p className={tw`text-dark-text text-sm`}>
+          <p className={tw`text-light-text dark:text-dark-text text-sm`}>
             Every bit of your donation helps me create better content, ease my
             pain away, avoid asteroid mass extinction, and repel the upcoming
             alien invasion!
           </p>
-          <p className={tw`text-dark-text text-sm`}>
+          <p className={tw`text-light-text dark:text-dark-text text-sm`}>
             You can donate through Trakteer or Saweria with the buttons below:
           </p>
         </div>
         <div className={tw`flex flex-row gap-5`}>
           <a
             href="https://saweria.co/irvanmalik48"
-            className={tw`bg-dark-accent-quartertrans px-4 py-2 inline-block text-sm text-uppercase text-dark-text rounded-3xl transition-all duration-200 ease-out hover:bg-dark-accent-semitrans hover:text-dark-accent-solid ring ring-dark-accent-quartertrans hover:ring-dark-accent-solid font-bold w-[fit-content] mt-5`}
+            className={tw`bg-light-accent-quartertrans dark:bg-dark-accent-quartertrans px-4 py-2 inline-block text-sm text-uppercase text-light-text dark:text-dark-text rounded-3xl transition-all duration-200 ease-out hover:bg-dark-accent-semitrans hover:text-light-accent-solid hover:dark:text-dark-accent-solid ring ring-transparent hover:ring-light-accent-solid hover:dark:ring-dark-accent-solid font-bold w-[fit-content] mt-5`}
           >
             Saweria
           </a>
           <a
             href="https://trakteer.id/lapprealm/tip"
-            className={tw`bg-dark-accent-quartertrans px-4 py-2 inline-block text-sm text-uppercase text-dark-text rounded-3xl transition-all duration-200 ease-out hover:bg-dark-accent-semitrans hover:text-dark-accent-solid ring ring-dark-accent-quartertrans hover:ring-dark-accent-solid font-bold w-[fit-content] mt-5`}
+            className={tw`bg-light-accent-quartertrans dark:bg-dark-accent-quartertrans px-4 py-2 inline-block text-sm text-uppercase text-light-text dark:text-dark-text rounded-3xl transition-all duration-200 ease-out hover:bg-dark-accent-semitrans hover:text-light-accent-solid hover:dark:text-dark-accent-solid ring ring-transparent hover:ring-light-accent-solid hover:dark:ring-dark-accent-solid font-bold w-[fit-content] mt-5`}
           >
             Trakteer
           </a>
         </div>
       </div>
       <div
-        className={tw`hidden lg:flex justify-center items-center w-[fit-content] h-full p-3 rounded-xl bg-dark-accent-semitrans text-dark-accent-solid`}
+        className={tw`hidden lg:flex justify-center items-center w-[fit-content] h-full p-3 rounded-xl bg-light-accent-semitrans dark:bg-dark-accent-semitrans text-light-accent-solid dark:text-dark-accent-solid`}
       >
         <CoffeeIcon />
       </div>
