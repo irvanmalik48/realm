b0c4bf0d648101a436872c9a462bf8fa487492a9
Irvan Malik <irvanmalik48@gmail.com>
2022-09-10 12:08:28 +0700

    feat(islands/fab): light mode
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- islands/FAB.tsx
+++ islands/FAB.tsx
@@ -29,7 +29,7 @@ export default function FAB() {
     <button
       className={tw`fixed ${
         isVisible ? "opacity-100" : "opacity-0 cursor-default"
-      } bottom-[7rem] right-[1rem] md:bottom-[3rem] md:right-[5rem] text-dark-text bg-dark-nav ring ring-dark-accent-semitrans shadow-lg text-dark-accent-solid w-[fit-content] p-4 rounded-full hover:bg-dark-accent-solid hover:ring-dark-accent-solid hover:shadow-xl hover:text-dark-nav transition-all duration-300 z-[999]`}
+      } bottom-[7rem] right-[1rem] md:bottom-[3rem] md:right-[5rem] bg-light-nav dark:bg-dark-nav ring ring-light-accent-semitrans dark:ring-dark-accent-semitrans shadow-lg text-light-accent-solid dark:text-dark-accent-solid w-[fit-content] p-4 rounded-full hover:bg-light-accent-solid hover:dark:bg-dark-accent-solid hover:ring-light-accent-solid hover:dark:ring-dark-accent-solid hover:shadow-xl hover:text-light-nav hover:dark:text-dark-nav transition-all duration-300 z-[999]`}
       aria-label="Back to top"
       onClick={(e) => {
         e.preventDefault;
