f8643203efa4dc0923171d3ee22334deca25ac5c
Irvan Malik <irvanmalik48@gmail.com>
2022-09-11 18:35:13 +0700

    refactor(components/nav): refactor nav items
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -7,6 +7,7 @@ import {
   RepoIcon,
 } from "@components/Icons.tsx";
 import { tw } from "@utils/twind.ts";
+import { NavItem } from "@components/NavItem.tsx";
 
 export default function Nav(props: { active: string }) {
   return (
@@ -16,118 +17,68 @@ export default function Nav(props: { active: string }) {
       <div
         className={tw`grid grid-cols-4 md:flex md:flex-col w-full md:w-auto justify-center items-center gap-2 md:gap-5`}
       >
-        <a
-          href="/"
-          className={tw`w-full md:w-auto flex flex-row justify-center md:justify-start items-center gap-4`}
+        <NavItem
+          active="home"
+          parentActive={props.active}
+          href="/home"
+          text="Home"
         >
-          <div
-            className={tw`${
-              props.active === "home"
-                ? "md:bg-dark-accent-semitrans text-dark-accent-solid"
-                : ""
-            } w-full md:w-auto group flex flex-col justify-center items-center bg-transparent text-dark-text w-[fit-content] p-3 rounded-xl md:hover:rounded-3xl hover:bg-dark-accent-solid hover:text-dark-nav transition-all duration-300`}
-          >
-            <HomeIcon />
-            <span
-              className={tw`block group-hover:text-dark-nav md:group-hover:text-dark-text md:group-hover:scale-100 md:bg-dark-nav md:absolute md:left-24 font-semibold md:m-2 w-auto md:min-w-max md:origin-left md:scale-0 md:rounded-3xl md:uppercase md:px-6 md:py-2 text-xs md:text-sm transition-all duration-100`}
-            >
-              Home
-            </span>
-          </div>
-        </a>
-        <a
+          <HomeIcon />
+        </NavItem>
+        <NavItem
+          active="posts"
+          parentActive={props.active}
           href="/posts"
-          className={tw`w-full md:w-auto flex flex-row justify-center md:justify-start items-center gap-4`}
+          text="Posts"
         >
-          <div
-            className={tw`${
-              props.active === "posts"
-                ? "md:bg-dark-accent-semitrans text-dark-accent-solid"
-                : ""
-            } w-full md:w-auto group flex flex-col justify-center items-center bg-transparent text-dark-text w-[fit-content] p-3 rounded-xl md:hover:rounded-3xl hover:bg-dark-accent-solid hover:text-dark-nav transition-all duration-300`}
-          >
-            <BlogIcon />
-            <span
-              className={tw`block group-hover:text-dark-nav md:group-hover:text-dark-text md:group-hover:scale-100 md:bg-dark-nav md:absolute md:left-24 font-semibold md:m-2 w-auto md:min-w-max md:origin-left md:scale-0 md:rounded-3xl md:uppercase md:px-6 md:py-2 text-xs md:text-sm transition-all duration-100`}
-            >
-              Posts
-            </span>
-          </div>
-        </a>
-        <a
+          <BlogIcon />
+        </NavItem>
+        <NavItem
+          active="projects"
+          parentActive={props.active}
           href="/projects"
-          className={tw`w-full md:w-auto flex flex-row justify-center md:justify-start items-center gap-4`}
-        >
-          <div
-            className={tw`${
-              props.active === "projects"
-                ? "md:bg-dark-accent-semitrans text-dark-accent-solid"
-                : ""
-            } w-full md:w-auto group flex flex-col justify-center items-center bg-transparent text-dark-text w-[fit-content] p-3 rounded-xl md:hover:rounded-3xl hover:bg-dark-accent-solid hover:text-dark-nav transition-all duration-300`}
-          >
-            <ProjectIcon />
-            <span
-              className={tw`block group-hover:text-dark-nav md:group-hover:text-dark-text md:group-hover:scale-100 md:bg-dark-nav md:absolute md:left-24 font-semibold md:m-2 w-auto md:min-w-max md:origin-left md:scale-0 md:rounded-3xl md:uppercase md:px-6 md:py-2 text-xs md:text-sm transition-all duration-100`}
-            >
+          text={
+            <>
               Projects
               <span className={tw`hidden md:inline`}>{" "}(WIP)</span>
-            </span>
-          </div>
-        </a>
-        <a
-          href="/about"
-          className={tw`w-full md:w-auto flex flex-row justify-center md:justify-start items-center gap-4`}
+            </>
+          }
         >
-          <div
-            className={tw`${
-              props.active === "about"
-                ? "md:bg-dark-accent-semitrans text-dark-accent-solid"
-                : ""
-            } w-full md:w-auto group flex flex-col justify-center items-center bg-transparent text-dark-text w-[fit-content] p-3 rounded-xl md:hover:rounded-3xl hover:bg-dark-accent-solid hover:text-dark-nav transition-all duration-300`}
-          >
-            <AboutIcon />
-            <span
-              className={tw`block group-hover:text-dark-nav md:group-hover:text-dark-text md:group-hover:scale-100 md:bg-dark-nav md:absolute md:left-24 font-semibold md:m-2 w-auto md:min-w-max md:origin-left md:scale-0 md:rounded-3xl md:uppercase md:px-6 md:py-2 text-xs md:text-sm transition-all duration-100`}
-            >
-              About
-            </span>
-          </div>
-        </a>
+          <ProjectIcon />
+        </NavItem>
+        <NavItem
+          active="about"
+          parentActive={props.active}
+          href="/about"
+          text="About"
+        >
+          <AboutIcon />
+        </NavItem>
       </div>
       <div
         className={tw`hidden md:flex md:flex-col w-full md:w-auto justify-center items-center gap-2 md:gap-5`}
       >
-        <a
+        <NavItem
+          active="no-active"
+          parentActive={props.active}
           href="https://github.com/irvanmalik48/realm"
-          className={tw`w-full md:w-auto flex flex-row justify-center md:justify-start items-center gap-4`}
+          text="This Blog's Repository"
         >
-          <div
-            className={tw`w-full md:w-auto group flex flex-col justify-center items-center bg-transparent text-dark-text w-[fit-content] p-3 rounded-xl md:hover:rounded-3xl hover:bg-dark-accent-solid hover:text-dark-nav transition-all duration-300`}
-          >
-            <RepoIcon />
-            <span
-              className={tw`block group-hover:text-dark-nav md:group-hover:text-dark-text md:group-hover:scale-100 md:bg-dark-nav md:absolute md:left-24 font-semibold md:m-2 w-auto md:min-w-max md:origin-left md:scale-0 md:rounded-3xl md:uppercase md:px-6 md:py-2 text-xs md:text-sm text-dark-text transition-all duration-100`}
-            >
-              This Blog's Repository
-            </span>
-          </div>
-        </a>
-        <a
+          <RepoIcon />
+        </NavItem>
+        <NavItem
+          active="no-active"
+          parentActive={props.active}
           href="https://github.com/irvanmalik48"
-          className={tw`md:flex flex-row justify-center md:justify-start items-center gap-4 hidden`}
-        >
-          <div
-            className={tw`group flex justify-center items-center bg-transparent text-dark-text w-[fit-content] p-3 rounded-xl hover:rounded-3xl hover:bg-dark-accent-solid hover:text-dark-nav transition-all duration-300`}
-          >
-            <GitHubIcon />
-            <span
-              className={tw`md:block hidden group-hover:scale-100 bg-dark-nav absolute left-24 font-semibold m-2 w-auto min-w-max origin-left scale-0 rounded-3xl uppercase px-6 py-2 text-sm text-dark-text transition-all duration-100`}
-            >
+          text={
+            <>
               Jump to my{" "}
               <span className={tw`text-dark-accent-solid`}>GitHub Profile</span>
-            </span>
-          </div>
-        </a>
+            </>
+          }
+        >
+          <GitHubIcon />
+        </NavItem>
       </div>
     </nav>
   );
