8ed9521094701bd5ec0f5b634893f4f994a679b2
Irvan Malik <irvanmalik48@gmail.com>
2022-09-15 14:01:17 +0700

    chore(main): rearrange imports
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- main.ts
+++ main.ts
@@ -7,8 +7,9 @@
 import { start } from "$fresh/server.ts";
 import manifest from "@/fresh.gen.ts";
 import { IS_BROWSER } from "$fresh/runtime.ts";
-
 import { config as loadEnv } from "$std/dotenv/mod.ts";
+import twind from "@utils/plugin-twind/mod.ts";
+import { config } from "@utils/twind.ts";
 
 if (!IS_BROWSER) {
   await loadEnv({
@@ -16,8 +17,5 @@ if (!IS_BROWSER) {
     allowEmptyValues: true,
   });
 }
-
-import twind from "@utils/plugin-twind/mod.ts";
-import { config } from "@utils/twind.ts";
 
 await start(manifest, { plugins: [twind(config)] });
