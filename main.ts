f528030b9afd1fc4f6bb5c8f25446da2515a7ea5
Irvan Malik <irvanmalik48@gmail.com>
2022-09-15 14:00:07 +0700

    feat(main): use `IS_BROWSER` checking to determine if it's in Deno Deploy or local
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- main.ts
+++ main.ts
@@ -6,8 +6,16 @@
 
 import { start } from "$fresh/server.ts";
 import manifest from "@/fresh.gen.ts";
+import { IS_BROWSER } from "$fresh/runtime.ts";
 
-import "$std/dotenv/load.ts";
+import { config as loadEnv } from "$std/dotenv/mod.ts";
+
+if (!IS_BROWSER) {
+  await loadEnv({
+    export: true,
+    allowEmptyValues: true,
+  });
+}
 
 import twind from "@utils/plugin-twind/mod.ts";
 import { config } from "@utils/twind.ts";
