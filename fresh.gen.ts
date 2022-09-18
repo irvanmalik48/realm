fe0c858c218d6cd0fbcdc550e015dd7d742790ab
Irvan Malik <irvanmalik48@gmail.com>
2022-09-18 22:43:02 +0700

    codegen: chore update
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -5,16 +5,18 @@
 import config from "./deno.json" assert { type: "json" };
 import * as $0 from "./routes/_404.tsx";
 import * as $1 from "./routes/about.tsx";
-import * as $2 from "./routes/api/post.ts";
-import * as $3 from "./routes/bg.svg.tsx";
-import * as $4 from "./routes/blooper.tsx";
-import * as $5 from "./routes/index.tsx";
-import * as $6 from "./routes/jokes.tsx";
-import * as $7 from "./routes/posts.tsx";
-import * as $8 from "./routes/posts/[slug].tsx";
-import * as $9 from "./routes/sitemap.xml.tsx";
-import * as $10 from "./routes/sw-register.tsx";
-import * as $11 from "./routes/sw.tsx";
+import * as $2 from "./routes/api/index.ts";
+import * as $3 from "./routes/api/post.ts";
+import * as $4 from "./routes/api/postmd.ts";
+import * as $5 from "./routes/bg.svg.tsx";
+import * as $6 from "./routes/blooper.tsx";
+import * as $7 from "./routes/index.tsx";
+import * as $8 from "./routes/jokes.tsx";
+import * as $9 from "./routes/posts.tsx";
+import * as $10 from "./routes/posts/[slug].tsx";
+import * as $11 from "./routes/sitemap.xml.tsx";
+import * as $12 from "./routes/sw-register.tsx";
+import * as $13 from "./routes/sw.tsx";
 import * as $$0 from "./islands/CopyButton.tsx";
 import * as $$1 from "./islands/CounterLogic.tsx";
 import * as $$2 from "./islands/FAB.tsx";
@@ -24,16 +26,18 @@ const manifest = {
   routes: {
     "./routes/_404.tsx": $0,
     "./routes/about.tsx": $1,
-    "./routes/api/post.ts": $2,
-    "./routes/bg.svg.tsx": $3,
-    "./routes/blooper.tsx": $4,
-    "./routes/index.tsx": $5,
-    "./routes/jokes.tsx": $6,
-    "./routes/posts.tsx": $7,
-    "./routes/posts/[slug].tsx": $8,
-    "./routes/sitemap.xml.tsx": $9,
-    "./routes/sw-register.tsx": $10,
-    "./routes/sw.tsx": $11,
+    "./routes/api/index.ts": $2,
+    "./routes/api/post.ts": $3,
+    "./routes/api/postmd.ts": $4,
+    "./routes/bg.svg.tsx": $5,
+    "./routes/blooper.tsx": $6,
+    "./routes/index.tsx": $7,
+    "./routes/jokes.tsx": $8,
+    "./routes/posts.tsx": $9,
+    "./routes/posts/[slug].tsx": $10,
+    "./routes/sitemap.xml.tsx": $11,
+    "./routes/sw-register.tsx": $12,
+    "./routes/sw.tsx": $13,
   },
   islands: {
     "./islands/CopyButton.tsx": $$0,
