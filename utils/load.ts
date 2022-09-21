6fdcab76f3b5ff8ab0f18387a37c483541df4433
Irvan Malik <irvanmalik48@gmail.com>
2022-09-21 11:25:48 +0700

    feat(utils/load): add function to load project md
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- utils/load.ts
+++ utils/load.ts
@@ -1,7 +1,7 @@
 import { extract as parse } from "$std/encoding/front_matter.ts";
 import { relative } from "$std/path/posix.ts";
 import { walk } from "$std/fs/walk.ts";
-import { Post } from "@/types.d.tsx";
+import { Post, Projects } from "@/types.d.tsx";
 import { Any } from "any";
 
 export async function loadPost(
@@ -54,6 +54,62 @@ export async function loadContent(postsDirectory: string) {
     }
   }
   return posts;
+}
+
+export async function loadProject(
+  postsDirectory: string,
+  path: string,
+): Promise<[string, Projects]> {
+  let contents = "";
+  try {
+    contents = await Deno.readTextFile(path);
+  } catch (_e) {
+    return [
+      path,
+      {
+        title: "Undefined",
+        desc: "Undefined",
+        screenshot: "/showcase/this-blog.webp",
+        stack: ["Undefined"],
+        path: path,
+        link: "#",
+        gh: "#",
+        md: "Undefined",
+      },
+    ];
+  }
+
+  let pathname = "/" + relative(postsDirectory, path);
+  pathname = pathname.split(".")[0];
+
+  const { body, attrs } = parse<Record<string, Any>>(contents);
+
+  const projects: Projects = {
+    title: attrs.title,
+    desc: attrs.desc,
+    stack: attrs.stack,
+    path: attrs.path ?? pathname,
+    screenshot: attrs.screenshot,
+    link: attrs.link,
+    gh: attrs.gh,
+    md: body,
+  };
+
+  return [pathname, projects];
+}
+
+export async function loadShowcases(projectsDirectory: string) {
+  const projectList = new Map<string, Projects>();
+  for await (const entry of walk(projectsDirectory)) {
+    if (entry.isFile && entry.path.endsWith(".md")) {
+      const [key, projects]: [string, Projects] = await loadProject(
+        projectsDirectory,
+        entry.path,
+      );
+      projectList.set(key, projects);
+    }
+  }
+  return projectList;
 }
 
 export function timeToRead(data: Post) {
