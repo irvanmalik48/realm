18bb414b1d86a8b465ce53371a9a5165090774e1
Irvan Malik <irvanmalik48@gmail.com>
2022-10-01 03:02:40 +0700

    chore(components/projectcard): deno fmt
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -13,12 +13,24 @@ export default function ProjectCard(props: {
       href={props.path}
       className={tw`ring ring-transparent overflow-hidden flex flex-col justify-between block w-full bg-dark-accent-quartertrans rounded-xl hover:bg-dark-accent-semitrans hover:ring-dark-accent-solid transition-all duration-200 ease-out text-dark-text box-border`}
     >
-      <img src={asset(props.img as string)} alt="project image" className={tw`w-full h-auto`} />
+      <img
+        src={asset(props.img as string)}
+        alt="project image"
+        className={tw`w-full h-auto`}
+      />
       <div className={tw`pb-3`}>
-        <p className={tw`text-dark-accent-solid text-lg pt-3 px-5 font-semibold font-heading`}>
+        <p
+          className={tw`text-dark-accent-solid text-lg pt-3 px-5 font-semibold font-heading`}
+        >
           {props.title}
         </p>
-        <p className={tw`text-dark-text px-5 text-sm mb-1 overflow-clip max-h-[1.25rem] ${lineClamp(1)}`}>{props.desc}</p>
+        <p
+          className={tw`text-dark-text px-5 text-sm mb-1 overflow-clip max-h-[1.25rem] ${
+            lineClamp(1)
+          }`}
+        >
+          {props.desc}
+        </p>
       </div>
     </a>
   );
