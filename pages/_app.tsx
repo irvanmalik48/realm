8002caae4c3268a300778165a60f51b109651f1e
Irvan Malik <irvanmalik48@gmail.com>
2022-10-30 02:27:30 +0700

    feat(pages/_app): add tags
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,6 +18,28 @@ export default function App({ Component, pageProps }: AppProps) {
           cardType: "summary_large_image",
         }}
         canonical="https://www.irvanma.me/"
+        additionalLinkTags={[
+          {
+            rel: "icon",
+            href: "/favicon.ico",
+          },
+          {
+            rel: "icon",
+            href: "/icons/android-chrome-192x192.png",
+          },
+          {
+            rel: "icon",
+            href: "/icons/android-chrome-512x512.png",
+          },
+          {
+            rel: "apple-touch-icon",
+            href: "/icons/apple-touch-icon.png",
+          },
+          {
+            rel: "manifest",
+            href: "/site.webmanifest",
+          },
+        ]}
       />
       <Component {...pageProps} />
     </>
