7474a6bcdc9012fc6cf1070bca4b170df806cc58
Irvan Malik <irvanmalik48@gmail.com>
2022-10-30 02:46:32 +0700

    feat(components/layout/post): yeah uhh
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/layouts/PostLayout.tsx
+++ components/layouts/PostLayout.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import BaseLayout from "./BaseLayout";
 
 const ResponsiveImage = (props: any) => {
-  return <Image alt={props.alt} layout="responsive" {...props} />;
+  return <Image className="w-full" alt={props.alt} layout="responsive" {...props} />;
 };
 
 const components = { img: ResponsiveImage };
