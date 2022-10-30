cd3826b6c32eb012596510d543eeec1600dd5bde
Irvan Malik <irvanmalik48@gmail.com>
2022-10-30 14:40:58 +0700

    feat(components/layout/base): i'm too bothered to know
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/layouts/BaseLayout.tsx
+++ components/layouts/BaseLayout.tsx
@@ -1,7 +1,7 @@
-import Head from "next/head";
 import Footer from "../stateless/Footer";
 import Navbar from "../stateful/Navbar";
 import { NextSeo } from "next-seo";
+import ScrollToTop from "../stateful/ScrollToTop";
 
 export default function BaseLayout(props: any) {
   return (
@@ -19,6 +19,8 @@ export default function BaseLayout(props: any) {
       <Navbar />
       <main>{props.children}</main>
       <Footer />
+      <div className="fixed bottom-0 left-0 right-0 z-40 w-full h-24 md:h-16 lg:h-12 bg-gradient-to-t from-gray-900 to-transparent"></div>
+      <ScrollToTop />
     </>
   );
 }
