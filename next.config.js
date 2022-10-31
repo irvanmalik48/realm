8d35695717ab9f31804987f05a8e70b4eceb3f5f
Irvan Malik <irvanmalik48@gmail.com>
2022-11-01 01:42:11 +0700

    feat(config): alias to preact
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- next.config.js
+++ next.config.js
@@ -2,6 +2,19 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+        "react/jsx-runtime": "preact/jsx-runtime",
+        "react-ssr-prepass": "preact-ssr-prepass",
+        "react-render-to-string": "preact-render-to-string",
+      });
+    }
+    return config;
+  },
   pageExtensions: ["ts", "tsx", "js", "jsx"],
   images: {
     domains: ["github.com", "www.github.com"],
