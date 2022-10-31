5b8f71c776474af32a2e71ff2089b08b4080d4f4
Irvan Malik <irvanmalik48@gmail.com>
2022-11-01 01:42:26 +0700

    chore(components/layouts/base): move navbar to _app
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/layouts/BaseLayout.tsx
+++ components/layouts/BaseLayout.tsx
@@ -1,5 +1,4 @@
 import Footer from "../stateless/Footer";
-import Navbar from "../stateful/Navbar";
 import { NextSeo } from "next-seo";
 import ScrollToTop from "../stateful/ScrollToTop";
 
@@ -16,10 +15,8 @@ export default function BaseLayout(props: any) {
           description: props.description,
         }}
       />
-      <Navbar />
       <main>{props.children}</main>
       <Footer />
-      <div className="fixed bottom-0 left-0 right-0 z-40 w-full h-24 md:h-16 lg:h-12 bg-gradient-to-t from-gray-900 to-transparent"></div>
       <ScrollToTop />
     </>
   );
