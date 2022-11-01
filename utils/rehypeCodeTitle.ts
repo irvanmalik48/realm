34978c5fed25994f1e5b13c5627cd9767f89fa15
Irvan Malik <irvanmalik48@gmail.com>
2022-11-02 01:02:10 +0700

    feat(utils): add rehype plugin
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

---
+++ utils/rehypeCodeTitle.ts
@@ -0,0 +1,27 @@
+import { visit } from "unist-util-visit";
+import { h } from "hastscript";
+
+export default function codeTitle(opt: any) {
+  const className = (opt && opt.className) || "rehype-code-title";
+
+  return function (tree: any) {
+    visit(tree, { tagName: "pre" }, visitor);
+
+    function visitor(node: any, index: any) {
+      if (node.children && node.children.length > 0) {
+        const { properties } = node.children[0];
+        if (properties.className && properties.className.length > 0) {
+          const [lang, filename] = properties.className[0]
+            .split(":")
+            .map((e: any) => e.trim());
+          properties.className = lang;
+          if (!filename) return;
+
+          const title = h("h1", filename);
+          const container = h("div", { class: className }, [title, node]);
+          tree.children[index] = container;
+        }
+      }
+    }
+  };
+}
