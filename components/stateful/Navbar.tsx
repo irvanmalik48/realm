24de51159e71edaa614ed760a8e3863b2228086f
Irvan Malik <irvanmalik48@gmail.com>
2022-11-03 14:46:27 +0700

    fix(components/stateful/navbar): fix unhighlighted nav items
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/stateful/Navbar.tsx
+++ components/stateful/Navbar.tsx
@@ -8,17 +8,17 @@ export default function Navbar(props: any) {
 
   useEffect(() => {
     setMatchUrl(router.asPath);
-  }, [router.asPath]);
+  }, [router]);
 
   return (
     <nav className="fixed bottom-4 left-4 lg:bottom-1/2 lg:translate-y-1/2 xl:left-8 z-50">
       <div className="stack hover:before:bg-red-400 hover:before:bg-opacity-40 before:transition rounded-md relative p-2 bg-gray-800 border-2 border-red-400 before:absolute before:inset-0 before:bg-gray-700">
         <div className="flex items-center justify-center lg:flex-col gap-2">
           <Link
-            className={
-              "leading-none hover:bg-red-400 flex items-center hover:bg-opacity-80 transition ease-out bg-gray-700 p-3 rounded group " +
-              (matchUrl === "/" ? "bg-red-400 bg-opacity-80" : "")
-            }
+            className="leading-none hover:bg-red-400 flex items-center hover:bg-opacity-80 transition ease-out p-3 rounded group"
+            style={{
+              background: matchUrl === "/" ? "rgb(248 113 113 / 0.8)" : "rgb(55 65 81 / 1)",
+            }}
             href={"/"}
           >
             <span className="material-symbols-sharp">home</span>
@@ -27,10 +27,10 @@ export default function Navbar(props: any) {
             </span>
           </Link>
           <Link
-            className={
-              "leading-none hover:bg-red-400 flex items-center hover:bg-opacity-80 transition ease-out bg-gray-700 p-3 rounded group " +
-              (matchUrl === "/posts" ? "bg-red-400 bg-opacity-80" : "")
-            }
+            className="leading-none hover:bg-red-400 flex items-center hover:bg-opacity-80 transition ease-out p-3 rounded group"
+            style={{
+              background: matchUrl === "/posts" ? "rgb(248 113 113 / 0.8)" : "rgb(55 65 81 / 1)",
+            }}
             href={"/posts"}
           >
             <span className="material-symbols-sharp">pages</span>
@@ -39,10 +39,10 @@ export default function Navbar(props: any) {
             </span>
           </Link>
           <Link
-            className={
-              "leading-none hover:bg-red-400 flex items-center hover:bg-opacity-80 transition ease-out bg-gray-700 p-3 rounded group " +
-              (matchUrl === "/projects" ? "bg-red-400 bg-opacity-80" : "")
-            }
+            className="leading-none hover:bg-red-400 flex items-center hover:bg-opacity-80 transition ease-out p-3 rounded group"
+            style={{
+              background: matchUrl === "/projects" ? "rgb(248 113 113 / 0.8)" : "rgb(55 65 81 / 1)",
+            }}
             href={"/projects"}
           >
             <span className="material-symbols-sharp">work</span>
@@ -51,10 +51,10 @@ export default function Navbar(props: any) {
             </span>
           </Link>
           <Link
-            className={
-              "leading-none hover:bg-red-400 flex items-center hover:bg-opacity-80 transition ease-out bg-gray-700 p-3 rounded group " +
-              (matchUrl === "/about" ? "bg-red-400 bg-opacity-80" : "")
-            }
+            className="leading-none hover:bg-red-400 flex items-center hover:bg-opacity-80 transition ease-out p-3 rounded group"
+            style={{
+              background: matchUrl === "/about" ? "rgb(248 113 113 / 0.8)" : "rgb(55 65 81 / 1)",
+            }}
             href={"/about"}
           >
             <span className="material-symbols-sharp">info</span>
