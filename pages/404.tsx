375d16845752424c101c4fee73e5d3d841c15cab
Irvan Malik <irvanmalik48@gmail.com>
2023-01-08 02:59:46 +0700

    chore(404): yeah miss the the
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- pages/404.tsx
+++ pages/404.tsx
@@ -8,7 +8,7 @@ export default function NotFound() {
   return (
     <div className="bg-neutral-900 w-full px-5 relative">
       <Head>
-        <title>Realm | 404</title>
+        <title>The Realm | 404</title>
       </Head>
       <img
         src="/misc/lottie.svg"
