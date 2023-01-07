5a7f70b136602a5fc5fb74331edf1c57ee177a97
Irvan Malik <irvanmalik48@gmail.com>
2023-01-08 02:59:19 +0700

    feat(404): properly give title
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,11 +1,15 @@
 import { FAB } from "c/FAB";
 import { Footer } from "c/Footer";
+import Head from "next/head";
 import React from "react";
 import "s/globals.css";
 
 export default function NotFound() {
   return (
     <div className="bg-neutral-900 w-full px-5 relative">
+      <Head>
+        <title>Realm | 404</title>
+      </Head>
       <img
         src="/misc/lottie.svg"
         className="w-48 absolute left-0 top-0 hidden lg:block"
