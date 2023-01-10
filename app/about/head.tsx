073ca00069beb478af5d3f98f5ce5062b18715f7
Irvan Malik <irvanmalik48@gmail.com>
2023-01-10 23:38:57 +0700

    feat(about): add about page
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

---
+++ app/about/head.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Head() {
+  return (
+    <>
+      <title>Realm | About</title>
+      <meta
+        name="viewport"
+        content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
+      />
+      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+    </>
+  );
+}
