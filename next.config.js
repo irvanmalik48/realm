c41d509f1faec1a1ad90f777633b609cd108e759
Irvan Malik <irvanmalik48@gmail.com>
2023-01-12 00:04:49 +0700

    feat(pwa): use pwa
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withPWA({
   reactStrictMode: true,
   experimental: {
     appDir: true,
   },
-};
+  productionBrowserSourceMaps: true,
+});
