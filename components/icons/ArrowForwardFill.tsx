9e81538a4763a1a8db4d9f3d41dbbad57920b518
Irvan Malik <irvanmalik48@gmail.com>
2023-01-12 23:43:44 +0700

    feat(components/icons): add SVG icons
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

---
+++ components/icons/ArrowForwardFill.tsx
@@ -0,0 +1,16 @@
+import { SVGProps } from "react";
+const SvgArrowForwardFill = (
+  props: SVGProps<SVGSVGElement> & { size: string }
+) => (
+  <div className={props.className}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={props.size}
+      height={props.size}
+      viewBox="0 0 48 48"
+    >
+      <path d="M22.95 38.9q-.4-.4-.4-1.05t.4-1.05l11.3-11.3H9.5q-.65 0-1.075-.425Q8 24.65 8 24q0-.65.425-1.075Q8.85 22.5 9.5 22.5h24.75l-11.3-11.3q-.4-.4-.4-1.075 0-.675.4-1.075.4-.4 1.05-.4t1.05.4l13.9 13.9q.25.25.35.5.1.25.1.55 0 .25-.1.525t-.35.525l-13.9 13.9q-.4.4-1.05.375-.65-.025-1.05-.425Z" />
+    </svg>
+  </div>
+);
+export default SvgArrowForwardFill;
