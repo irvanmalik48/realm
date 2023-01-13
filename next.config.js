94d62a87935d2c6bcffd26273455f02156237bbb
Irvan Malik <irvanmalik48@gmail.com>
2023-01-13 22:37:46 +0700

    feat(next): remove @plaiceholder/next
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- next.config.js
+++ next.config.js
@@ -3,16 +3,16 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   disable: process.env.NODE_ENV === "development",
 });
 
-const { withPlaiceholder } = require("@plaiceholder/next");
-
 /** @type {import('next').NextConfig} */
-module.exports = withPWA(withPlaiceholder({
-  reactStrictMode: true,
-  compiler: {
-    removeConsole: true,
-  },
-  experimental: {
-    appDir: true,
-  },
-  productionBrowserSourceMaps: true,
-}));
+module.exports = withPWA(
+  withPlaiceholder({
+    reactStrictMode: true,
+    compiler: {
+      removeConsole: true,
+    },
+    experimental: {
+      appDir: true,
+    },
+    productionBrowserSourceMaps: true,
+  })
+);
