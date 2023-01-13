6088c45c7e0af8cf4a59bd868e25e12ffcc6cf04
Irvan Malik <irvanmalik48@gmail.com>
2023-01-14 02:39:57 +0700

    chore(prettier): prettify
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- components/stateless/TrackScroll.tsx
+++ components/stateless/TrackScroll.tsx
@@ -16,12 +16,8 @@ export function TrackScroll() {
         className="h-1 w-full fixed top-0 left-0 bg-teal-300 z-[999]"
         style={{ scaleX }}
       />
-      <div
-        className="h-[40px] w-full fixed top-0 left-0 z-[5] bg-gradient-to-b from-neutral-900 to-transparent"
-      />
-      <div
-        className="h-[40px] w-full fixed bottom-0 left-0 z-[5] bg-gradient-to-t from-neutral-900 to-transparent"
-      />
+      <div className="h-[40px] w-full fixed top-0 left-0 z-[5] bg-gradient-to-b from-neutral-900 to-transparent" />
+      <div className="h-[40px] w-full fixed bottom-0 left-0 z-[5] bg-gradient-to-t from-neutral-900 to-transparent" />
     </>
   );
 }
