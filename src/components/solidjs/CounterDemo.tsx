44b6627c87ecd4757dea4fe109d0bd01485e1c2e
Irvan Malik <irvanmalik48@gmail.com>
2023-04-02 01:53:46 +0700

    feat(components/solidjs): move everything related to solidjs to their respective folder
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

---
+++ src/components/solidjs/CounterDemo.tsx
@@ -0,0 +1,23 @@
+import { createSignal } from "solid-js";
+
+export default function CounterDemo() {
+  const [count, setCount] = createSignal(0);
+
+  return (
+    <div class="flex justify-between items-center p-3 not-prose bg-neutral-800 rounded border-neutral-700 border">
+      <button
+        class="hover:bg-opacity-50 transition font-mono font-bold text-2xl bg-neutral-900 border-neutral-700 rounded border px-5 py-3"
+        onClick={() => setCount(count() - 1)}
+      >
+        -
+      </button>
+      <p class="w-full text-center text-4xl">{count()}</p>
+      <button
+        class="hover:bg-opacity-50 transition font-mono font-bold text-2xl bg-neutral-900 border-neutral-700 rounded border px-5 py-3"
+        onClick={() => setCount(count() + 1)}
+      >
+        +
+      </button>
+    </div>
+  );
+}
