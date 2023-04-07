b62b6ba56bab7297f2fe1583de973c5b5d78b6b8
Irvan Malik <irvanmalik48@gmail.com>
2023-04-08 00:17:53 +0700

    feat(components/navrail): add privacy policy and readjust the layouting
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- src/components/solidjs/NavRail.tsx
+++ src/components/solidjs/NavRail.tsx
@@ -6,13 +6,14 @@ import {
   Scale,
   Code2,
   Book,
+  ShieldCheck,
 } from "lucide-solid";
 
 export default function NavRail(props: any) {
   return (
     <nav
       id={props.id}
-      class="bg-neutral-800 h-fit md:h-screen fixed md:sticky md:top-0 bottom-0 py-3 px-4 md:px-3 md:py-3 w-full md:w-fit flex flex-row md:flex-col justify-center md:justify-between border-t md:border-t-0 md:border-r border-neutral-700 z-50"
+      class="bg-neutral-800 md:bg-opacity-100 bg-opacity-80 backdrop-blur-md h-fit md:h-screen fixed md:sticky md:top-0 bottom-0 py-3 px-4 md:px-3 md:py-3 w-full md:w-fit flex flex-row md:flex-col justify-center md:justify-between border-t md:border-t-0 md:border-r border-neutral-700 z-50"
     >
       <div class="flex flex-row justify-between md:justify-center w-full md:w-fit md:flex-col gap-3">
         <a
@@ -61,25 +62,25 @@ export default function NavRail(props: any) {
           <p class="nav-item-label">About</p>
         </a>
         <a
-          href="/creed"
+          href="/privacy-policy"
           class={`md:hidden group nav-item ${
-            props.path === "/creed" ? "nav-item-active" : ""
+            props.path === "/privacy-policy" ? "nav-item-active" : ""
           }`}
         >
-          <Scale size={18} />
-          <p class="nav-item-label">Journalist's Creed</p>
-        </a>
-        <a
-          href="/oath"
-          class={`md:hidden group nav-item ${
-            props.path === "/oath" ? "nav-item-active" : ""
-          }`}
-        >
-          <Code2 size={18} />
-          <p class="nav-item-label">Programmer's Oath</p>
+          <ShieldCheck size={18} />
+          <p class="nav-item-label">Privacy Policy</p>
         </a>
       </div>
       <div class="hidden md:flex flex-col gap-3">
+        <a
+          href="/privacy-policy"
+          class={`group nav-item ${
+            props.path === "/privacy-policy" ? "nav-item-active" : ""
+          }`}
+        >
+          <ShieldCheck size={18} />
+          <p class="nav-item-label">Privacy Policy</p>
+        </a>
         <a
           href="/creed"
           class={`group nav-item ${
