c38371f1ed7243e5cda5f4b103e49ba942997360
Irvan Malik <irvanmalik48@gmail.com>
2023-07-10 22:11:55 +0700

    fix(deployment): make sure the process always run server side
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- src/components/image/image.tsx
+++ src/components/image/image.tsx
@@ -1,18 +1,24 @@
 import { component$, useSignal, useTask$ } from "@builder.io/qwik";
+import { server$ } from "@builder.io/qwik-city";
 import { getPixels } from "@unpic/pixels";
 import { blurhashToCssGradientString } from "@unpic/placeholder";
 import { type ImageProps, Image as OptimizedImage } from "@unpic/qwik";
 import { encode } from "blurhash";
 
+const processImage = server$(async (imgUrl: string) => {
+  const heroData = await getPixels(imgUrl);
+
+  const data = Uint8ClampedArray.from(heroData.data);
+  const blurHash = encode(data, heroData.width, heroData.height, 4, 4);
+
+  return blurHash;
+})
+
 export const Image = component$<ImageProps>((props: ImageProps) => {
   const blurHashData = useSignal("");
 
   useTask$(async () => {
-    const heroData = await getPixels(props.src);
-
-    const data = Uint8ClampedArray.from(heroData.data);
-    const blurHash = encode(data, heroData.width, heroData.height, 4, 4);
-    blurHashData.value = blurhashToCssGradientString(blurHash);
+    blurHashData.value = blurhashToCssGradientString(await processImage(props.src));
   });
 
   return (
