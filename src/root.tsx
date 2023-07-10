bc505816dd73e7eca8a2ee2c6b905a58ee871b6d
Irvan Malik <irvanmalik48@gmail.com>
2023-07-10 20:57:54 +0700

    feat(root): update head childrens
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- src/root.tsx
+++ src/root.tsx
@@ -20,7 +20,10 @@ export default component$(() => {
     <QwikCityProvider>
       <head>
         <meta charSet="utf-8" />
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <RouterHead />
       </head>
       <body
