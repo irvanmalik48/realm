5167a80a96ee4e745a87ec6808ed3853eb96faa9
Irvan Malik <irvanmalik48@gmail.com>
2023-07-11 12:04:40 +0700

    feat(comp/header-hero): no anim, yeah
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- src/components/header-hero/header-hero.tsx
+++ src/components/header-hero/header-hero.tsx
@@ -1,44 +1,12 @@
-import { $, component$, useOnDocument } from "@builder.io/qwik";
+import { component$ } from "@builder.io/qwik";
 import { Image } from "../image/image";
 
 export default component$(() => {
-  useOnDocument(
-    "mousemove",
-    $((event) => {
-      const headerHero = document.getElementById("header-hero");
-      const interactionCanvas = document.getElementById("interaction-canvas");
-      const mouseEvent = event as MouseEvent;
-
-      if (!headerHero) return;
-      if (!interactionCanvas) return;
-
-      const x = mouseEvent.pageX - headerHero.offsetLeft / 2;
-      const y = mouseEvent.pageY - headerHero.offsetTop / 2;
-
-      // use the mouse position to change the radial gradient of the interaction canvas position
-      if (
-        x < 0 ||
-        y < 0 ||
-        x > headerHero.offsetWidth ||
-        y > headerHero.offsetHeight
-      ) {
-        interactionCanvas.style.opacity = "0";
-      } else {
-        interactionCanvas.style.opacity = "1";
-        interactionCanvas.style.background = `radial-gradient(circle at ${x}px ${y}px, #06b6d4df, #06b6d400)`;
-      }
-    })
-  );
-
   return (
     <div
       class="w-full h-auto rounded-xl bg-neutral-900 relative"
       id="header-hero"
     >
-      <div
-        class="absolute -inset-1 z-0 rounded-xl transition-opacity duration-700"
-        id="interaction-canvas"
-      />
       <Image
         src="https://cdn.realmof.tech/hero_rz1wup.jpg"
         layout="fullWidth"
