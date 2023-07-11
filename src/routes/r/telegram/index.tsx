87ba18c8909f201fc0bdb8d74005ae159f4f1a8d
Irvan Malik <irvanmalik48@gmail.com>
2023-07-11 21:06:01 +0700

    feat(routes/redirects): add telegram redirect
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

---
+++ src/routes/r/telegram/index.tsx
@@ -0,0 +1,64 @@
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead, RequestEvent } from "@builder.io/qwik-city";
+import { Waves } from "~/components/waves/waves";
+
+async function wait(s: number) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, s * 1000);
+  });
+}
+
+export const onGet = async ({ redirect }: RequestEvent) => {
+  await wait(3);
+  throw redirect(302, "https://t.me/lapplund");
+};
+
+export default component$(() => {
+  return (
+    <>
+      <section class="w-full py-24 grid grid-cols-1 place-content-center min-h-screen relative">
+        <div class="max-w-4xl w-full mx-auto px-5">
+          <h1 class="w-full text-neutral-100 font-semibold font-heading text-4xl md:text-5xl">
+            Redirecting to Telegram...
+          </h1>
+          <p class="w-full text-neutral-300 text-lg md:text-2xl mt-5 pb-10">
+            If you're not redirected automatically, click{" "}
+            <a
+              href="https://t.me/lapplund"
+              class="text-neutral-300 hover:text-neutral-100 transition underline"
+            >
+              here
+            </a>
+            .
+          </p>
+          <a
+            href="https://social.gnuweeb.org/@lappland"
+            rel="me"
+            class="hidden"
+          >
+            mastodon verification
+          </a>
+        </div>
+        <Waves showText={false} />
+      </section>
+    </>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "Telegram",
+  meta: [
+    {
+      name: "description",
+      content: "Redirecting",
+    },
+    {
+      name: "og:title",
+      content: "Telegram",
+    },
+    {
+      name: "og:description",
+      content: "Redirecting",
+    },
+  ],
+};
