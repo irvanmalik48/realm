f68f59aeae65c570f52ac09ba41f5d9edf945fc8
Irvan Malik <irvanmalik48@gmail.com>
2023-07-11 12:05:17 +0700

    chore(root): prettify
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- src/root.tsx
+++ src/root.tsx
@@ -21,9 +21,23 @@ export default component$(() => {
       <head>
         <meta charSet="utf-8" />
         <meta name="theme-color" content="#0A0A0A" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="manifest" href="/site.webmanifest" />
         <RouterHead />
       </head>
