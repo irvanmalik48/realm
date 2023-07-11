a1eb83569a673d5d50b811d1aa54d9a5025a9ea0
Irvan Malik <irvanmalik48@gmail.com>
2023-07-11 12:04:21 +0700

    chore(comp/image): prettify
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- src/components/image/image.tsx
+++ src/components/image/image.tsx
@@ -4,22 +4,21 @@ import { blurhashToCssGradientString } from "@unpic/placeholder";
 import { type ImageProps, Image as OptimizedImage } from "@unpic/qwik";
 
 const processImage = server$(async (imgUrl: string) => {
-  const blurhashFetch = await fetch(`${imgUrl}?fm=blurhash`).then((res) => res.text());
+  const blurhashFetch = await fetch(`${imgUrl}?fm=blurhash`).then((res) =>
+    res.text()
+  );
 
   return blurhashFetch;
-})
+});
 
 export const Image = component$<ImageProps>((props: ImageProps) => {
   const blurHashData = useSignal("");
 
   useTask$(async () => {
-    blurHashData.value = blurhashToCssGradientString(await processImage(props.src));
+    blurHashData.value = blurhashToCssGradientString(
+      await processImage(props.src)
+    );
   });
 
-  return (
-    <OptimizedImage
-      {...props}
-      background={blurHashData.value}
-    />
-  );
+  return <OptimizedImage {...props} background={blurHashData.value} />;
 });
