ae8b5874811111461e721a36c7403317ab600a8a
Irvan Malik <irvanmalik48@gmail.com>
2023-07-14 01:20:52 +0700

    fix(view-transition): try to fix the z-index
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- src/components/cursor/cursor.tsx
+++ src/components/cursor/cursor.tsx
@@ -112,7 +112,7 @@ export default component$(() => {
   return (
     <>
       <div
-        class="fixed z-[9999] pointer-events-none md:block hidden"
+        class="fixed z-[9999] pointer-events-none md:block hidden view-transition-target-cursor"
         id="cursor"
         style={{
           opacity: 0,
