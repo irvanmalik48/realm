90f7b1bfcaa5de35515b0d4727dc43e5758b5864
Irvan Malik <irvanmalik48@gmail.com>
2023-07-14 03:06:40 +0700

    feat(vite): god i hope this works
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,14 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { partytownVite } from "@builder.io/partytown/utils";
 import { join } from "path";
 import { qwikReact } from "@builder.io/qwik-react/vite";
+import wasm from "vite-plugin-wasm";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig(() => {
   return {
     plugins: [
+      wasm(),
+      topLevelAwait(),
       qwikCity(),
       qwikVite(),
       tsconfigPaths(),
