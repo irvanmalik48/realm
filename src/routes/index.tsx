c493c075991d106a3064c5410ad6cc612e954d0e
Irvan Malik <irvanmalik48@gmail.com>
2023-07-14 00:18:38 +0700

    refactor(routes/index): adhere to `max-len`
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { component$ } from "@builder.io/qwik";
+import { twMerge } from "tailwind-merge";
 import { Link, type DocumentHead } from "@builder.io/qwik-city";
 import { Image } from "~/components/image/image";
 import { Image as ImageNoBlurHash } from "@unpic/qwik";
@@ -50,7 +51,14 @@ export default component$(() => {
     <>
       <section class="w-full max-w-4xl px-5 py-24 mx-auto min-h-screen relative">
         <HeaderHero />
-        <div class="w-36 h-36 -mt-16 rounded-full bg-neutral-900 ring-4 ring-neutral-700 overflow-hidden z-20 relative mx-auto">
+        <div
+          class={twMerge(
+            "w-36 h-36 z-20",
+            "mx-auto -mt-16",
+            "rounded-full relative overflow-hidden bg-neutral-900",
+            "ring-4 ring-neutral-700"
+          )}
+        >
           <ImageNoBlurHash
             src="https://avatars.githubusercontent.com/u/71539547?v=4&size=250"
             layout="constrained"
@@ -60,13 +68,25 @@ export default component$(() => {
             class="w-full h-full"
           />
         </div>
-        <h1 class="md:text-4xl text-2xl font-semibold text-center font-heading text-neutral-100 mt-7">
+        <h1
+          class={twMerge(
+            "md:text-4xl text-2xl text-neutral-100",
+            "font-semibold text-center font-heading",
+            "mt-7"
+          )}
+        >
           Irvan Malik Azantha
         </h1>
         <p class="text-center md:text-xl text-neutral-300 md:mt-1">
           a weeb that just happens to know how to code
         </p>
-        <p class="w-fit mx-auto text-center text-sm text-neutral-300 mt-10 px-5 py-1 rounded-full mb-5 bg-neutral-900 bg-opacity-50">
+        <p
+          class={twMerge(
+            "w-fit mx-auto text-center text-sm",
+            "text-neutral-300 mt-10 px-5 py-1",
+            "rounded-full mb-5 bg-neutral-900 bg-opacity-50"
+          )}
+        >
           here's the menu:
         </p>
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3">
@@ -76,12 +96,23 @@ export default component$(() => {
               id={`${link.label.toLowerCase()}-card-link`}
               key={index}
               aria-labelledby={`${link.label}-icon-label`}
-              class="w-full relative flex flex-col gap-3 bg-opacity-70 rounded-xl px-5 py-3 bg-neutral-900 transition-[background-color_transform] hover:bg-neutral-800 hover:scale-105 group active:scale-95"
+              class={twMerge(
+                "w-full relative flex flex-col gap-3",
+                "bg-opacity-70 bg-neutral-900",
+                "rounded-xl px-5 py-3 hover:bg-neutral-800",
+                "hover:scale-105 group active:scale-95",
+                "transition-[background-color_transform]"
+              )}
             >
               {link.recommended && (
                 <div
                   title="Your donation would really help me!"
-                  class="absolute text-xs text-neutral-900 transition-colors group-hover:text-neutral-800 font-semibold rounded-full bg-cyan-500 right-3 top-3 p-2"
+                  class={twMerge(
+                    "absolute text-xs text-neutral-900",
+                    "transition-colors group-hover:text-neutral-800",
+                    "font-semibold rounded-full bg-cyan-500",
+                    "right-3 top-3 p-2"
+                  )}
                 >
                   <LuThumbsUp class="w-4 h-4" />
                 </div>
@@ -101,17 +132,28 @@ export default component$(() => {
           <Link
             href={"#intro"}
             title="Scroll down to the introduction section"
-            class="w-fit absolute bottom-10 left-1/2 -translate-x-1/2 hover:scale-125 duration-500 transition active:scale-90 bg-neutral-800 rounded-full flex items-center justify-center p-5"
+            class={twMerge(
+              "w-fit absolute bottom-10 left-1/2 -translate-x-1/2 hover:scale-125",
+              "duration-500 transition active:scale-90 bg-neutral-800",
+              "rounded-full flex items-center justify-center p-5"
+            )}
           >
             <div class="w-4 h-4 rounded-full absolute -bottom-1.5 bg-cyan-500" />
-            <div class="w-4 h-4 rounded-full absolute -bottom-1.5 bg-cyan-500 animate-ping" />
+            <div
+              class={
+                "w-4 h-4 rounded-full absolute -bottom-1.5 bg-cyan-500 animate-ping"
+              }
+            />
             <LuArrowDown class="w-8 h-8" />
           </Link>
         </div>
       </section>
       <Marquees>
         <section
-          class="w-full relative text-neutral-100 py-28 min-h-screen bg-neutral-900 bg-opacity-20"
+          class={twMerge(
+            "w-full relative text-neutral-100 py-28 min-h-screen",
+            "bg-neutral-900 bg-opacity-20"
+          )}
           id="intro"
         >
           <div class="mx-auto max-w-4xl w-full relative z-10 py-12 px-5">
@@ -133,10 +175,18 @@ export default component$(() => {
           <Link
             href={"#stance"}
             title="Stance towards the retarded events"
-            class="w-fit absolute bottom-24 left-1/2 -translate-x-1/2 hover:scale-125 duration-500 transition active:scale-90 bg-neutral-800 rounded-full flex items-center justify-center p-5"
+            class={twMerge(
+              "w-fit absolute bottom-24 left-1/2 -translate-x-1/2 hover:scale-125",
+              "duration-500 transition active:scale-90 bg-neutral-800",
+              "rounded-full flex items-center justify-center p-5"
+            )}
           >
             <div class="w-4 h-4 rounded-full absolute -bottom-1.5 bg-red-400" />
-            <div class="w-4 h-4 rounded-full absolute -bottom-1.5 bg-red-400 animate-ping" />
+            <div
+              class={
+                "w-4 h-4 rounded-full absolute -bottom-1.5 bg-red-400 animate-ping"
+              }
+            />
             <LuArrowDown class="w-8 h-8" />
           </Link>
         </section>
@@ -145,8 +195,17 @@ export default component$(() => {
         class="w-full relative text-neutral-100 py-28 my-20 min-h-screen"
         id="stance"
       >
-        <div class="hidden flex-col xl:flex absolute top-12 left-10 items-center w-[400px] gap-5">
-          <div class="absolute left-0 inset-y-0 w-56 bg-gradient-to-r from-neutral-950 to-transparent z-10" />
+        <div
+          class={
+            "hidden flex-col xl:flex absolute top-12 left-10 items-center w-[400px] gap-5"
+          }
+        >
+          <div
+            class={twMerge(
+              "absolute left-0 inset-y-0 w-56 bg-gradient-to-r",
+              "from-neutral-950 to-transparent z-10"
+            )}
+          />
           <Image
             src="https://cdn.realmof.tech/iraq-mosul_h0zwxg.jpg?w=0.6&expires=2019488400"
             alt="Mosul Devastated"
@@ -162,8 +221,16 @@ export default component$(() => {
             class="grayscale rounded-xl w-full object-cover brightness-50"
           />
         </div>
-        <div class="hidden flex-col xl:flex absolute bottom-12 right-10 items-center w-[400px] gap-5">
-          <div class="absolute right-0 inset-y-0 w-56 bg-gradient-to-l from-neutral-950 to-transparent z-10" />
+        <div
+          class={
+            "hidden flex-col xl:flex absolute bottom-12 right-10 items-center w-[400px] gap-5"
+          }
+        >
+          <div
+            class={
+              "absolute right-0 inset-y-0 w-56 bg-gradient-to-l from-neutral-950 to-transparent z-10"
+            }
+          />
           <Image
             src="https://cdn.realmof.tech/ukraine-war_vqtmtr.jpg?w=0.75&expires=2019488400"
             alt="Ukraine War"
@@ -179,7 +246,13 @@ export default component$(() => {
             class="grayscale rounded-xl w-full object-cover brightness-50"
           />
         </div>
-        <div class="mx-auto max-w-4xl w-full relative z-10 py-8 px-5 rounded-2xl bg-neutral-950 bg-opacity-30 backdrop-blur-md">
+        <div
+          class={twMerge(
+            "mx-auto max-w-4xl w-full relative z-10",
+            "py-8 px-5 rounded-2xl bg-neutral-950 bg-opacity-30",
+            "backdrop-blur-md"
+          )}
+        >
           <h1 class="text-3xl md:text-5xl lg:text-6xl font-medium font-heading">
             Stance towards geopolitical
             <br />
@@ -210,7 +283,12 @@ export default component$(() => {
             <span class="text-neutral-100">open</span> to anything. Just don't
             be a d*ck.
           </p>
-          <blockquote class="text-xl md:text-3xl mt-10 text-neutral-100 bg-neutral-900 pt-5 pb-2 w-full rounded-xl">
+          <blockquote
+            class={twMerge(
+              "text-xl md:text-3xl mt-10 text-neutral-100",
+              "bg-neutral-900 pt-5 pb-2 w-full rounded-xl"
+            )}
+          >
             <p class="px-8 py-2 w-fit mx-auto text-heading rounded-full bg-neutral-950">
               Ignorance is bliss.
             </p>
