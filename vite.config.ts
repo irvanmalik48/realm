1ebeeb535e04e5d3d9d3b0c751865abc39c86ab4
Irvan Malik <irvanmalik48@gmail.com>
2023-07-14 15:38:16 +0700

    feat(vite): syntax highlighting
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,35 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { partytownVite } from "@builder.io/partytown/utils";
 import { join } from "path";
 
-export default defineConfig(() => {
+export default defineConfig(async () => {
+  const { default: rehypePrettyCode } = await import("rehype-pretty-code");
+
   return {
     plugins: [
-      qwikCity(),
+      qwikCity({
+        mdxPlugins: {
+          rehypeSyntaxHighlight: false,
+          remarkGfm: true,
+          rehypeAutolinkHeadings: true,
+        },
+        mdx: {
+          rehypePlugins: [[
+            rehypePrettyCode,
+            {
+              theme: "dark-plus",
+              keepBackground: false,
+              onVisitLine(node: any) {
+                if (node.children.length === 0) {
+                  node.children = [{ type: 'text', value: ' ' }];
+                }
+              },
+              onVisitHighlightedLine(node: any) {
+                node.properties.className.push('line--highlighted');
+              },
+            }
+          ]],
+        }
+      }),
       qwikVite(),
       tsconfigPaths(),
       partytownVite({ dest: join(__dirname, "dist", "~partytown") }),
