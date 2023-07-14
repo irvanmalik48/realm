0548a4f0f6991991570f63e89ca2e2198f8e6e79
Irvan Malik <irvanmalik48@gmail.com>
2023-07-14 23:03:08 +0700

    feat(csp): add a csp
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

---
+++ src/routes/plugin@csp.ts
@@ -0,0 +1,21 @@
+import type { RequestHandler } from "@builder.io/qwik-city";
+import { isDev } from "@builder.io/qwik/build";
+
+export const onRequest: RequestHandler = event => {
+  if (isDev) return;
+
+  const nonce = atob(crypto.randomUUID().split("-").join(""));
+  event.sharedMap.set("@nonce", nonce);
+  const csp = [
+    `default-src 'self' 'unsafe-inline'`,
+    `font-src 'self'`,
+    `img-src 'self' 'unsafe-inline' data: https://cdn.realmof.tech`,
+    `script-src 'self' 'unsafe-inline' https: 'nonce-${nonce}' 'strict-dynamic'`,
+    `style-src 'self' 'unsafe-inline'`,
+    `frame-src 'self' 'nonce-${nonce}'`,
+    `object-src 'none'`,
+    `base-uri 'self'`,
+  ];
+
+  event.headers.set("Content-Security-Policy", csp.join("; "));
+};
