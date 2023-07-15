c6d682138791204ade21ebb8b6240f96454f909f
Irvan Malik <irvanmalik48@gmail.com>
2023-07-16 04:23:12 +0700

    feat(comp/header-hero): add a bit of CRT vibes to it
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- src/components/header-hero/header-hero.tsx
+++ src/components/header-hero/header-hero.tsx
@@ -4,9 +4,10 @@ import { Image } from "../image/image";
 export default component$(() => {
   return (
     <div
-      class="w-full h-auto rounded-xl bg-neutral-900 relative"
+      class="w-full h-auto rounded-xl bg-neutral-900 relative overflow-hidden ring-2 ring-neutral-900"
       id="header-hero"
     >
+      <div class="screen-overlay" />
       <Image
         src="https://cdn.realmof.tech/hero_rz1wup.jpg?w=0.6&expires=2019488400"
         width="1500"
