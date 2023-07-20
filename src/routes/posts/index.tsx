5d2b768d832f35974dc2475b343b21588b8d22b2
Irvan Malik <irvanmalik48@gmail.com>
2023-07-21 01:20:30 +0700

    feat(routes/posts): introduce a fuzzy search bar and revamp the post looks
    also implemented draft previews cause why not
    
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- src/routes/posts/index.tsx
+++ src/routes/posts/index.tsx
@@ -1,4 +1,9 @@
-import { Resource, component$, useResource$ } from "@builder.io/qwik";
+import {
+  Resource,
+  component$,
+  useResource$,
+  useSignal,
+} from "@builder.io/qwik";
 import {
   Link,
   type DocumentHeadProps,
@@ -6,40 +11,36 @@ import {
 } from "@builder.io/qwik-city";
 import { twMerge } from "tailwind-merge";
 import { asyncMap } from "~/lib/asyncmap";
-
-type PostSummary = {
-  title: string;
-  desc: string;
-  date: string;
-  permalink: string;
-  tags: string[];
-  draft: boolean;
-};
-
-export const getPosts = async (): Promise<PostSummary[]> => {
-  const modules = import.meta.glob("/src/routes/**/**/index.mdx");
-  const posts = await asyncMap(Object.keys(modules), async (path) => {
-    const data = (await modules[path]()) as DocumentHeadProps;
-
-    return {
-      title: data.head.title,
-      desc: data.head.frontmatter.desc,
-      date: data.head.frontmatter.date,
-      permalink: data.head.frontmatter.permalink,
-      tags: data.head.frontmatter.tags,
-      draft: data.head.frontmatter.draft,
-    };
-  });
-
-  return posts
-    .filter((post) => !post.draft)
-    .sort((a, b) => {
-      return new Date(b.date).getTime() - new Date(a.date).getTime();
-    });
-};
+import type { Post } from "~/types/definitions";
+import { searchPost } from "~/lib/fuzzy";
 
 export default component$(() => {
-  const postRes = useResource$(getPosts);
+  const searchingValue = useSignal<string>("");
+
+  const postRes = useResource$(async ({ track }): Promise<Post[]> => {
+    const searchValue = track(() => searchingValue.value);
+    const modules = import.meta.glob("/src/routes/**/**/index.mdx");
+    const posts = await asyncMap(Object.keys(modules), async (path) => {
+      const data = (await modules[path]()) as DocumentHeadProps;
+
+      return {
+        title: data.head.title,
+        desc: data.head.frontmatter.desc,
+        date: data.head.frontmatter.date,
+        permalink: data.head.frontmatter.permalink,
+        tags: data.head.frontmatter.tags,
+        draft: data.head.frontmatter.draft,
+      };
+    });
+
+    if (searchValue !== "") {
+      return searchPost(searchValue, posts);
+    }
+
+    return posts.sort((a, b) => {
+      return new Date(b.date).getTime() - new Date(a.date).getTime();
+    });
+  });
 
   return (
     <>
@@ -48,42 +49,30 @@ export default component$(() => {
           Blog posts
         </h1>
         <p class="mt-5 text-neutral-300 text-lg">All that I've written.</p>
+        <input
+          type="text"
+          placeholder="Search posts..."
+          class={twMerge(
+            "mt-10 bg-neutral-900 rounded-full w-full px-5 py-3",
+            "focus:bg-neutral-800 text-neutral-300 placeholder-neutral-400",
+            "placeholder:italic focus:outline-none focus:ring-2",
+            "focus:ring-cyan-500 transition my-5"
+          )}
+          bind:value={searchingValue}
+        />
         <Resource
           value={postRes}
           onResolved={(posts) => (
-            <div
-              class={
-                "flex flex-col mt-10 border-y border-neutral-800 divide-y divide-neutral-800"
-              }
-            >
-              {posts.map((post) => (
-                <Link
-                  key={post.permalink}
-                  href={post.permalink}
-                  class="flex flex-col p-5 hover:bg-neutral-900 hover:bg-opacity-20 transition"
-                >
-                  <h2 class="text-xl font-medium font-heading truncate">
-                    {post.title}
-                  </h2>
-                  <p class="mt-2 text-neutral-300 truncate">{post.desc}</p>
-                  <p class="mt-2 text-neutral-300 text-sm">{post.date}</p>
-                  {post.tags.length > 0 && (
-                    <div class="flex flex-row flex-wrap mt-2">
-                      {post.tags.map((tag) => (
-                        <span
-                          key={tag}
-                          class={twMerge(
-                            "px-3 py-1 mr-2 bg-neutral-800",
-                            "rounded-full text-neutral-300 text-xs"
-                          )}
-                        >
-                          {tag}
-                        </span>
-                      ))}
-                    </div>
-                  )}
-                </Link>
-              ))}
+            <div class="flex flex-col gap-5">
+              {import.meta.env.PUBLIC_ENV === "development" &&
+                posts
+                  .filter((post) => post.draft)
+                  .map((post) => <PostCard key={post.permalink} {...post} />)}
+              {posts
+                .filter((post) => !post.draft)
+                .map((post) => (
+                  <PostCard key={post.permalink} {...post} />
+                ))}
             </div>
           )}
         />
@@ -105,6 +94,49 @@ export default component$(() => {
         </p>
       </section>
     </>
+  );
+});
+
+const PostCard = component$((props: Post) => {
+  return (
+    <Link
+      key={props.permalink}
+      href={props.permalink}
+      class={twMerge(
+        "flex flex-col p-5 bg-neutral-900 rounded-xl",
+        "group hover:bg-neutral-800 transition relative"
+      )}
+    >
+      <h2 class="text-xl font-medium font-heading truncate">{props.title}</h2>
+      <p class="mt-2 text-neutral-300 truncate">{props.desc}</p>
+      <p class="mt-2 text-neutral-300 text-sm">{props.date}</p>
+      {props.tags.length > 0 && (
+        <div class="flex flex-row flex-wrap mt-2">
+          {props.tags.map((tag) => (
+            <span
+              key={tag}
+              class={twMerge(
+                "px-3 py-1 mr-2 bg-neutral-800 group-hover:bg-neutral-700",
+                "rounded-full text-neutral-300 text-xs transition"
+              )}
+            >
+              {tag}
+            </span>
+          ))}
+          {props.draft && (
+            <span
+              class={twMerge(
+                "px-3 py-1 mr-2 bg-yellow-400 group-hover:bg-yellow-300",
+                "rounded-full text-neutral-900 text-xs transition"
+              )}
+              title="This post is still a draft and will not be shown in production"
+            >
+              draft
+            </span>
+          )}
+        </div>
+      )}
+    </Link>
   );
 });
 
