4c918b4ecd88fde54e5b7d890abfbb6c6b41ea0b
Irvan Malik <irvanmalik48@gmail.com>
2023-07-22 18:59:25 +0700

    feat(data/projects): add project constants (just starters, i'm lazy to get all of my finished projects)
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

---
+++ src/data/projects/constants.ts
@@ -0,0 +1,75 @@
+import type { ProjectArray } from "~/types/definitions";
+
+export const PROJECT_CONSTANTS: ProjectArray = [
+  {
+    title: "Realm",
+    description:
+      "My personal portfolio and blogging website, built with Qwik and Tailwind. Intended as a canvas for my creativity.",
+    image: "https://cdn.realmof.tech/realm_project_maemve.png",
+    visibility: "public",
+    deployment: "Vercel (Edge)",
+    links: {
+      github: "https://github.com/irvanmalik48/realm",
+      deployed: "https://realmof.tech",
+    },
+  },
+  {
+    title: "SiFresh Backend",
+    description:
+      "A simple backend for SiFresh, built with Express and Typescript. Made as a Bangkit Academy 2023 Capstone Project deliverables.",
+    image: "https://cdn.realmof.tech/sifresh_bangkit_owgkfa.png",
+    visibility: "public",
+    deployment: "Google Cloud Platform (App Engine)",
+    links: {
+      github:
+        "https://github.com/vickysalim/BangkitCapstone-CS23-PS315/tree/main/cloud-computing",
+    },
+  },
+  {
+    title: "SIYANMAT",
+    description:
+      "A (not quite) simple web application for inventory management and reporting built with Next.js, Prisma, and Tailwind.",
+    image: "https://cdn.realmof.tech/siyanmat_project_xuxnv9.png",
+    visibility: "public",
+    deployment: "None (Private)",
+    links: {
+      github: "https://github.com/irvanmalik48/siyanmat-infolahtadam",
+    },
+  },
+  {
+    title: "Next.js Blog",
+    description:
+      "My past personal blog, built with Next.js and Tailwind. It's a bit messy, but it's a good learning experience.",
+    image: "https://cdn.realmof.tech/old_blog_project_t92e3z.png",
+    visibility: "public",
+    deployment: "Vercel (Serverless)",
+    links: {
+      github: "https://github.com/irvanmalik48/blog",
+      deployed: "https://old-realm-blog.vercel.app/",
+    },
+  },
+  {
+    title: "Three.js Portal",
+    description:
+      "A simple website showcasing Three.js and WebGL. Made with Next.js and Tailwind for a university final assignment.",
+    image: "https://cdn.realmof.tech/portal_project_xdhpsu.png",
+    visibility: "public",
+    deployment: "Vercel (Serverless)",
+    links: {
+      github: "https://github.com/irvanmalik48/portal",
+      deployed: "https://portal-realm.vercel.app/",
+    },
+  },
+  {
+    title: "React + TS + Redux Todo App",
+    description:
+      "A simple todo app built with React, Typescript, and Redux. Made as a learning experience for Redux.",
+    image: "https://cdn.realmof.tech/todo_project_ksfurr.png",
+    visibility: "public",
+    deployment: "Vercel (Serverless)",
+    links: {
+      github: "https://todo.realmof.tech/",
+      deployed: "https://todo.realmof.tech/",
+    },
+  },
+];
