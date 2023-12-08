6a327a5bfa7e17dcae251bbf5657eddfd3670793
Irvan Malik <irvanmalik48@gmail.com>
2023-12-09 00:43:57 +0700

    feat(pages/_app): add FAB and Toaster to topmost
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
+import FAB from "@/components/custom/fab";
 import { ThemeProvider } from "@/components/misc/theme-provider";
+import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import "@/styles/globals.css";
+import { AnimatePresence } from "framer-motion";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <ThemeProvider
       attribute="class"
@@ -11,7 +14,17 @@ export default function App({ Component, pageProps }: AppProps) {
       disableTransitionOnChange
     >
       <TooltipProvider>
-        <Component {...pageProps} />
+        <Toaster />
+        <FAB />
+        <AnimatePresence
+          mode="wait"
+          onExitComplete={() => {
+            window.scrollTo({ top: 0, behavior: "smooth" });
+          }}
+          initial={false}
+        >
+          <Component key={router.pathname} {...pageProps} />
+        </AnimatePresence>
       </TooltipProvider>
     </ThemeProvider>
   );
