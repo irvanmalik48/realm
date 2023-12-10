4687a08a1060fe3350b7520d18673a507f9e7a03
Irvan Malik <irvanmalik48@gmail.com>
2023-12-10 22:22:31 +0700

    feat(pages/_app): when smooth scrolling is off, try to scroll to top after animation is complete
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,14 @@ import FAB from "@/components/custom/fab";
 import { ThemeProvider } from "@/components/misc/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { useSSToggle } from "@/hooks/atoms";
 import "@/styles/globals.css";
 import { AnimatePresence } from "framer-motion";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps, router }: AppProps) {
+  const { smoothScrolling } = useSSToggle();
+
   return (
     <ThemeProvider
       attribute="class"
@@ -16,7 +19,18 @@ export default function App({ Component, pageProps, router }: AppProps) {
       <TooltipProvider>
         <Toaster />
         <FAB />
-        <AnimatePresence mode="wait" initial={false}>
+        <AnimatePresence
+          mode="wait"
+          initial={false}
+          onExitComplete={() => {
+            if (!smoothScrolling) {
+              window.scrollTo({
+                top: 0,
+                behavior: "auto",
+              });
+            }
+          }}
+        >
           <Component key={router.pathname} {...pageProps} />
         </AnimatePresence>
       </TooltipProvider>
