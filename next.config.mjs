45390ca2e01517b4705560ecc4548853c5a9af81
Irvan Malik <irvanmalik48@gmail.com>
2024-01-24 03:07:00 +0700

    revert(next): nah, no next/mdx for now
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,7 @@
-import createMDX from "@next/mdx";
-import remarkGfm from "remark-gfm";
-
-const withMDX = createMDX({
-  extension: /\.r?mdx?$/,
-  remarkPlugins: [remarkGfm],
-  rehypePlugins: [],
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "mdx", "rmd", "ts", "tsx"],
   reactStrictMode: true,
 };
 
-export default withMDX(nextConfig);
+export default nextConfig;
