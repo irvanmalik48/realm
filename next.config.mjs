6639c510e7609e0109d86203a566efd3a85e5464
Irvan Malik <irvanmalik48@gmail.com>
2024-03-15 13:22:46 +0700

    deps(remove): i regret
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,7 @@
-import withSerwistInit from "@serwist/next";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "mdx", "rmd", "ts", "tsx"],
   reactStrictMode: true,
 };
 
-const withSerwist = withSerwistInit({
-  swSrc: "sw/index.ts",
-  swDest: "public/sw.js",
-});
-
-export default withSerwist(nextConfig);
+export default nextConfig;
