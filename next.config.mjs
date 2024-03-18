04c154bd22cd372a56fc60fe3b8234c4bfd5ecd0
Irvan Malik <irvanmalik48@gmail.com>
2024-03-18 12:37:06 +0700

    feat(pwa): add serwist back
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,21 @@
+import {
+  PHASE_DEVELOPMENT_SERVER,
+  PHASE_PRODUCTION_BUILD,
+} from "next/constants.js";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "mdx", "rmd", "ts", "tsx"],
   reactStrictMode: true,
 };
 
-export default nextConfig;
+export default async (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER || phase === PHASE_PRODUCTION_BUILD) {
+    const withSerwist = (await import("@serwist/next")).default({
+      swSrc: "sw/index.ts",
+      swDest: "public/sw.js",
+    });
+    return withSerwist(nextConfig);
+  }
+  return nextConfig;
+};
