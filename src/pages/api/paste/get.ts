bb8a064f04dadaafeffa77d6f3ec9de3815b7218
Irvan Malik <irvanmalik48@gmail.com>
2024-03-18 14:55:33 +0700

    feat(pages/api/paste): add integration for vercel KV based pastebin-like service
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

---
+++ src/pages/api/paste/get.ts
@@ -0,0 +1,39 @@
+import { kv } from "@vercel/kv";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  const { id } = request.query;
+  let bintext;
+
+  if (request.method !== "GET") {
+    return response.status(405).json({
+      message: "Method Not Allowed",
+    });
+  }
+
+  try {
+    if (typeof id !== "string") {
+      throw new Error("Invalid ID.");
+    }
+
+    if (!id) {
+      throw new Error("ID is required.");
+    }
+
+    bintext = await kv.get(id).catch((error) => {
+      throw new Error((error as any).message);
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: (error as any).message,
+    });
+  }
+
+  return response.status(200).json({
+    id: id,
+    text: bintext,
+  });
+}
