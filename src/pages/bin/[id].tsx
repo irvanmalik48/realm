627265e18898cd4a103d05552b65c6ee028ed68e
Irvan Malik <irvanmalik48@gmail.com>
2024-04-08 17:39:31 +0700

    feat(pages/bin/[id]): use syntax highlighter and add relevant new features
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- src/pages/bin/[id].tsx
+++ src/pages/bin/[id].tsx
@@ -1,4 +1,10 @@
+import HighlightBin from "@/components/custom/highlight-bin";
 import DefaultLayout from "@/components/layout/default";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { useToast } from "@/components/ui/use-toast";
+import { CheckIcon, CodeIcon, CopyIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
 
 type ResultProp = {
   id: string;
@@ -25,10 +31,64 @@ export async function getServerSideProps({
 }
 
 export default function PasteBin({ data }: { data: ResultProp }) {
+  const { toast } = useToast();
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(data.text);
+    setCopied(true);
+
+    toast({
+      title: "Copied!",
+      description: "Successfully copied to your clipboard.",
+    });
+
+    const timeout = setTimeout(() => {
+      setCopied(false);
+      clearTimeout(timeout);
+    }, 2000);
+  };
+
   return (
     <DefaultLayout title={data.id} description="Your pasted bin.">
-      <div className="w-full bg-background text-foreground px-5 py-3 overflow-auto font-mono whitespace-pre">
-        {data.text}
+      <div className="w-full min-h-screen bg-background text-foreground p-0">
+        <div className="w-full flex justify-between items-center px-5 py-3 border-b border-border">
+          <Badge>{data.id}</Badge>
+          <div className="flex items-center gap-3">
+            <Button
+              size="sm"
+              className="flex items-center gap-2"
+              variant="outline"
+              onClick={handleCopy}
+            >
+              {copied ? (
+                <>
+                  <CheckIcon className="w-4 h-4" />
+                  Copied Bin!
+                </>
+              ) : (
+                <>
+                  <CopyIcon className="w-4 h-4" />
+                  Copy Bin
+                </>
+              )}
+            </Button>
+            <Button asChild size="sm" variant="outline">
+              <a
+                href={`${
+                  process.env.NODE_ENV !== "development"
+                    ? "https://irvanma.eu.org"
+                    : "http://localhost:3000"
+                }/api/paste/get?id=${data.id}&raw=true`}
+                className="flex items-center gap-2"
+              >
+                <CodeIcon className="w-4 h-4" />
+                View Raw
+              </a>
+            </Button>
+          </div>
+        </div>
+        <HighlightBin text={data.text} />
       </div>
     </DefaultLayout>
   );
