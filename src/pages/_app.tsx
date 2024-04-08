d548292a8fc338375a748981b56a7d235972354b
Irvan Malik <irvanmalik48@gmail.com>
2024-04-08 15:58:32 +0700

    rm(pages/_app): remove residues
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,11 @@ import FAB from "@/components/custom/fab";
 import { ThemeProvider } from "@/components/misc/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { useSSToggle } from "@/hooks/atoms";
 import "@/styles/globals.css";
 import { AnimatePresence } from "framer-motion";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps, router }: AppProps) {
-  const { smoothScrolling } = useSSToggle();
-
   return (
     <ThemeProvider
       attribute="class"
@@ -19,18 +16,7 @@ export default function App({ Component, pageProps, router }: AppProps) {
       <TooltipProvider>
         <Toaster />
         <FAB />
-        <AnimatePresence
-          mode="wait"
-          initial={false}
-          onExitComplete={() => {
-            if (!smoothScrolling) {
-              window.scrollTo({
-                top: 0,
-                behavior: "auto",
-              });
-            }
-          }}
-        >
+        <AnimatePresence mode="wait" initial={false}>
           <Component key={router.pathname} {...pageProps} />
         </AnimatePresence>
       </TooltipProvider>
