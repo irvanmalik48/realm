d36f233f252aac8662d74d2c23a4cb2f211156b9
Irvan Malik <irvanmalik48@gmail.com>
2024-04-08 18:35:07 +0700

    fix(hooks/highlight): fine tune languages loaded
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- src/hooks/highlight.ts
+++ src/hooks/highlight.ts
@@ -5,6 +5,27 @@ const useHighlight = (text: string) => {
   const [highlighted, setHighlighted] = useState<string>("");
   const [language, setLanguage] = useState<string>("plaintext");
 
+  highlight.configure({
+    languages: [
+      "javascript",
+      "typescript",
+      "css",
+      "c",
+      "cpp",
+      "java",
+      "kotlin",
+      "python",
+      "rust",
+      "go",
+      "bash",
+      "php",
+      "ruby",
+      "html",
+      "json",
+      "plaintext",
+    ],
+  });
+
   useEffect(() => {
     const highlightResult = highlight.highlightAuto(text);
 
