fd0a84888f7305703a7287ad4ae89e0670745c3c
Irvan Malik <irvanmalik48@gmail.com>
2024-04-08 17:50:01 +0700

    feat(comp/custom/highlight-bin): mount the component first
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- src/components/custom/highlight-bin.tsx
+++ src/components/custom/highlight-bin.tsx
@@ -2,35 +2,42 @@ import useHighlight from "@/hooks/highlight";
 import InjectCSS from "./inject-css";
 import { cn } from "@/lib/utils";
 import { getStyleClass, getTheme } from "@/lib/style-classes";
-import { memo } from "react";
+import { memo, useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 
 function HighlightBin({ text }: { text: string }) {
   const { highlighted, language } = useHighlight(text);
+  const [mounted, setMounted] = useState(false);
   const { theme } = useTheme();
 
+  useEffect(() => {
+    setMounted(true);
+  });
+
   return (
-    <>
-      <InjectCSS
-        css={getTheme(
-          theme === "dark" ? "StackoverflowDark" : "StackoverflowLight"
-        )}
-      />
-      <pre
-        className={cn(
-          getStyleClass(
+    mounted && (
+      <>
+        <InjectCSS
+          css={getTheme(
             theme === "dark" ? "StackoverflowDark" : "StackoverflowLight"
-          ),
-          "w-full overflow-auto font-mono whitespace-pre"
-        )}
-      >
-        <code
-          className={`hljs language-${language} !bg-background !px-5 !py-3 block`}
-          data-type="realm-syntax-highlighter"
-          dangerouslySetInnerHTML={{ __html: highlighted }}
+          )}
         />
-      </pre>
-    </>
+        <pre
+          className={cn(
+            getStyleClass(
+              theme === "dark" ? "StackoverflowDark" : "StackoverflowLight"
+            ),
+            "w-full overflow-auto font-mono whitespace-pre"
+          )}
+        >
+          <code
+            className={`hljs language-${language} !bg-background !px-5 !py-3 block`}
+            data-type="realm-syntax-highlighter"
+            dangerouslySetInnerHTML={{ __html: highlighted }}
+          />
+        </pre>
+      </>
+    )
   );
 }
 
