f12e0672a3ca2657bafc63e3b8f73bda2b34aa15
Irvan Malik <irvanmalik48@gmail.com>
2024-04-08 17:37:33 +0700

    feat(comp/custom/inject-css): add css injection component
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

---
+++ src/components/custom/inject-css.tsx
@@ -0,0 +1,33 @@
+import { FC, useState, useEffect } from "react";
+import { appendStyle, isDOMReady } from "@/lib/utils";
+
+const idCache: any = {};
+
+type InjectCSSProps = {
+  css: string;
+};
+
+function InjectCSS({ css }: InjectCSSProps) {
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    if (!idCache[css]) {
+      idCache[css] =
+        "__react_highlight_syntax__inject_css_id-" +
+        Object.keys(idCache).length;
+    }
+
+    if (isDOMReady()) {
+      appendStyle(idCache[css], css);
+      setReady(true);
+    }
+
+    if (!ready && isDOMReady()) {
+      appendStyle(idCache[css], css);
+    }
+  }, [css]);
+
+  return null;
+}
+
+export default InjectCSS;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,21 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function appendStyle(id: string, css: string) {
+  if (!document.head.querySelector("#" + id)) {
+    const node = document.createElement("style");
+    node.textContent = css;
+    node.id = id;
+
+    document.head.appendChild(node);
+  }
+}
+
+export function isDOMReady() {
+  return (
+    typeof window !== "undefined" &&
+    typeof document !== "undefined" &&
+    document.head
+  );
+}
