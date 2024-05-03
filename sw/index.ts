a3f217e208569f3bc0197d8a28a5c4ab4bafc481
Irvan Malik <irvanmalik48@gmail.com>
2024-05-03 23:38:06 +0700

    refactor(sw): import order refactor
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- sw/index.ts
+++ sw/index.ts
@@ -1,6 +1,6 @@
 import { defaultCache } from "@serwist/next/worker";
-import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
 import { Serwist } from "serwist";
+import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
 
 declare global {
   interface WorkerGlobalScope extends SerwistGlobalConfig {
