3f44f5b693bb496d3cf228d1f28161e8530f8d01
Irvan Malik <irvanmalik48@gmail.com>
2024-07-06 15:41:54 +0700

    feat(rss): yeah
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -5,10 +5,6 @@ import { getCollection } from "astro:content";
 export async function GET(context) {
   const blog = (await getCollection("blog")).filter((post) => !post.data.draft);
 
-  const projects = (await getCollection("projects")).filter(
-    (project) => !project.data.draft,
-  );
-
   const items = [...blog, ...projects].sort(
     (a, b) => new Date(b.data.date).valueOf() - new Date(a.data.date).valueOf(),
   );
