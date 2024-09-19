d4c47bd672437739c8c5c064b0d9a68b3658363d
Irvan Malik <irvanma@gnuweeb.org>
2024-09-19 23:41:33 +0700

    fix(build): fix derp
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,36 +1,36 @@
-import { defineCollection, z } from "astro:content";
+import { defineCollection, z } from 'astro:content'
 
 const blog = defineCollection({
-  type: "content",
+  type: 'content',
   schema: ({ image }) =>
     z.object({
       title: z
         .string()
         .max(
           60,
-          "Title should be 60 characters or less for optimal Open Graph display."
+          'Title should be 60 characters or less for optimal Open Graph display.',
         ),
       description: z
         .string()
         .max(
           155,
-          "Description should be 155 characters or less for optimal Open Graph display."
+          'Description should be 155 characters or less for optimal Open Graph display.',
         ),
       date: z.coerce.date(),
       image: image()
         .refine((img) => img.width === 1200 && img.height === 630, {
           message:
-            "The image must be exactly 1200px × 630px for Open Graph requirements.",
+            'The image must be exactly 1200px × 630px for Open Graph requirements.',
         })
         .optional(),
       tags: z.array(z.string()).optional(),
       authors: z.array(z.string()).optional(),
       draft: z.boolean().optional(),
     }),
-});
+})
 
-const authors = defineCollection({
-  type: "content",
+const webring = defineCollection({
+  type: 'content',
   schema: z.object({
     name: z.string(),
     webringType: z.string().optional(),
@@ -43,10 +43,10 @@ const authors = defineCollection({
     linkedin: z.string().url().optional(),
     discord: z.string().url().optional(),
   }),
-});
+})
 
 const projects = defineCollection({
-  type: "content",
+  type: 'content',
   schema: ({ image }) =>
     z.object({
       name: z.string(),
@@ -54,10 +54,10 @@ const projects = defineCollection({
       tags: z.array(z.string()),
       image: image().refine((img) => img.width === 1200 && img.height === 630, {
         message:
-          "The image must be exactly 1200px × 630px for Open Graph requirements.",
+          'The image must be exactly 1200px × 630px for Open Graph requirements.',
       }),
       link: z.string().url(),
     }),
-});
+})
 
-export const collections = { blog, authors, projects };
+export const collections = { blog, webring, projects }
