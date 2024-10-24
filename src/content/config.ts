5aeb9491b2f2a1cefe264d74f03973ed598ef472
Irvan Malik <irvanma@gnuweeb.org>
2024-10-24 19:26:36 +0700

    feat(pages/about): revamp about page
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- src/content/config.ts
+++ src/content/config.ts
@@ -8,13 +8,13 @@ const blog = defineCollection({
         .string()
         .max(
           60,
-          "Title should be 60 characters or less for optimal Open Graph display."
+          "Title should be 60 characters or less for optimal Open Graph display.",
         ),
       description: z
         .string()
         .max(
           155,
-          "Description should be 155 characters or less for optimal Open Graph display."
+          "Description should be 155 characters or less for optimal Open Graph display.",
         ),
       date: z.coerce.date(),
       image: image()
@@ -46,19 +46,4 @@ const webring = defineCollection({
   }),
 });
 
-const projects = defineCollection({
-  type: "content",
-  schema: ({ image }) =>
-    z.object({
-      name: z.string(),
-      description: z.string(),
-      tags: z.array(z.string()),
-      image: image().refine((img) => img.width === 1200 && img.height === 630, {
-        message:
-          "The image must be exactly 1200px × 630px for Open Graph requirements.",
-      }),
-      link: z.string().url(),
-    }),
-});
-
-export const collections = { blog, webring, projects };
+export const collections = { blog, webring };
