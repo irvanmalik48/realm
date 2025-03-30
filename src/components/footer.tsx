de352916d99efa42d0d5f2c86c94f98cf4f3bf29
Irvan Malik <irvanma@gnuweeb.org>
2025-03-30 17:51:12 +0700

    feat(comp/footer): adjust footer
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,9 @@
-import { Logo } from "./logo";
+import { RealmMoe } from "./realm-moe";
 
 export function Footer() {
   return (
-    <footer className="w-full max-w-3xl mx-auto p-5 gap-8 flex flex-col justify-center items-center">
-      <Logo className="size-24" />
+    <footer className="w-full max-w-3xl mb-30 md:mb-0 mx-auto p-5 gap-8 flex flex-col justify-center items-center">
+      <RealmMoe className="md:h-64 hover:scale-120 transition-all" />
       <p className="text-primary/75 text-sm text-center w-full">
         &copy; 2025 Irvan Malik Azantha. Licensed in RCCL.
       </p>
