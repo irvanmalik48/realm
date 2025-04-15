e1fdd209eb44c50245eeee352b148b47e022102a
Irvan Malik <irvanma@gnuweeb.org>
2025-04-15 23:28:30 +0700

    feat(comp/image): add image component
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

---
+++ src/components/image.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useState } from "react";
+
+import Image from "next/image";
+import {
+  StaticImageData,
+  type StaticImport,
+} from "next/dist/shared/lib/get-img-props";
+
+export interface ImageProps {
+  img: StaticImport;
+  alt: string;
+  className?: string;
+  height?: number;
+}
+
+export function ImageComponent({ img, alt, height, className }: ImageProps) {
+  const [isImageLoading, setIsImageLoading] = useState(true);
+
+  const image = img as StaticImageData;
+
+  return (
+    <div className={`relative overflow-clip ${className}`}>
+      <Image
+        src={image}
+        alt={alt}
+        height={height ?? image.height}
+        placeholder="blur"
+        blurDataURL={image.blurDataURL}
+        onLoad={() => setIsImageLoading(false)}
+        className={`${
+          isImageLoading ? "blur scale-150" : "remove-blur scale-100"
+        } transition-all ease-[cubic-bezier(0.22,_1,_0.36,_1)] duration-500`}
+      />
+    </div>
+  );
+}
