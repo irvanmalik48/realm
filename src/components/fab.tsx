e5aa725920204f7bb118915c2043c3e2747a36e7
Irvan Malik <irvanma@gnuweeb.org>
2025-04-17 23:33:22 +0700

    feat(comp/fab): add link to server status page
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- src/components/fab.tsx
+++ src/components/fab.tsx
@@ -13,6 +13,7 @@ import {
   Linkedin,
   MessageSquareMore,
   Send,
+  Server,
   Twitter,
   X,
 } from "lucide-react";
@@ -123,6 +124,16 @@ export function FAB() {
           >
             <Home className="size-4" />
             <span>Home</span>
+            <div className="absolute opacity-0 translate-x-1/2 right-4 top-1/2 -translate-y-1/2 group-hover:opacity-100 group-hover:translate-x-0 transition-all">
+              <ArrowRight className="size-4" />
+            </div>
+          </Link>
+          <Link
+            className="group relative border-b border-border text-sm cursor-pointer flex items-center gap-3 px-4 py-2 hover:bg-secondary hover:text-secondary-foreground transition-colors"
+            href="/status"
+          >
+            <Server className="size-4" />
+            <span>Server Status</span>
             <div className="absolute opacity-0 translate-x-1/2 right-4 top-1/2 -translate-y-1/2 group-hover:opacity-100 group-hover:translate-x-0 transition-all">
               <ArrowRight className="size-4" />
             </div>
