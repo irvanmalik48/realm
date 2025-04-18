3797a1e9619b6c2c01359a8335432df1a323ecd6
Irvan Malik <irvanma@gnuweeb.org>
2025-04-19 02:22:10 +0700

    feat(next): enable webpack memory optimizations and disable source map generation for both client and server
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   experimental: {
     reactCompiler: true,
+    webpackMemoryOptimizations: true,
+    serverSourceMaps: false,
   },
+  productionBrowserSourceMaps: false,
 };
 
 export default nextConfig;
