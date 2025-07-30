b9f6bced621a154ccb0c364b20e63f8839ddc28c
Irvan Malik <irvanma@gnuweeb.org>
2025-07-31 01:36:28 +0700

    feat(comp/minecraft): add bedrock
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- src/components/minecraft.tsx
+++ src/components/minecraft.tsx
@@ -29,7 +29,7 @@ export function ServerResponseItem(props: {
       <div
         className={cn(
           "w-full px-5 py-3 gap-3 flex items-center",
-          props.leftClassName,
+          props.leftClassName
         )}
       >
         <props.icon className="size-5" />
@@ -38,7 +38,7 @@ export function ServerResponseItem(props: {
       <div
         className={cn(
           "w-full px-5 py-3 flex items-center",
-          props.rightClassName,
+          props.rightClassName
         )}
       >
         <p>{props.content}</p>
@@ -49,9 +49,16 @@ export function ServerResponseItem(props: {
 
 async function getMinecraftServerStatus() {
   const data = await fetch(`https://api.mcsrvstat.us/3/mc.irvanma.eu.org`);
+  const dataBedrock = await fetch(
+    `https://api.mcsrvstat.us/bedrock/3/mc.irvanma.eu.org`
+  );
   const res: MinecraftServerStatusResponse = await data.json();
+  const resBedrock: MinecraftServerStatusResponse = await dataBedrock.json();
 
-  return res;
+  return {
+    java: res,
+    bedrock: resBedrock,
+  };
 }
 
 function checkIfOnline(query: MinecraftServerStatusResponse) {
@@ -68,52 +75,121 @@ export function MinecraftServer() {
   });
 
   return (
-    <div className="p-5 pt-3">
-      <div className="flex items-center justify-between mb-3">
-        <h3 className="text-primary font-bold dark:font-semibold flex items-center gap-3">
-          <Activity className="size-5" />
-          <span>Server Status</span>
-        </h3>
-        <p
-          className={cn(
-            "px-3 py-1 text-sm border rounded-full",
-            checkIfOnline(query.data as MinecraftServerStatusResponse)
-              ? "bg-green-800/20 border-green-800 dark:bg-green-500/20 dark:border-green-500 text-foreground"
-              : "bg-red-800/20 border-red-800 dark:bg-red-500/20 dark:border-red-500 text-foreground",
-          )}
-        >
-          {checkIfOnline(query.data as MinecraftServerStatusResponse)
-            ? "Online"
-            : "Offline"}
-        </p>
+    <>
+      <div className="p-5 pt-3">
+        <div className="flex items-center justify-between mb-3">
+          <h3 className="text-primary font-bold dark:font-semibold flex items-center gap-3">
+            <Activity className="size-5" />
+            <span>Server Status (Java)</span>
+          </h3>
+          <p
+            className={cn(
+              "px-3 py-1 text-sm border rounded-full",
+              checkIfOnline(query.data?.java as MinecraftServerStatusResponse)
+                ? "bg-green-800/20 border-green-800 dark:bg-green-500/20 dark:border-green-500 text-foreground"
+                : "bg-red-800/20 border-red-800 dark:bg-red-500/20 dark:border-red-500 text-foreground"
+            )}
+          >
+            {checkIfOnline(query.data?.java as MinecraftServerStatusResponse)
+              ? "Online"
+              : "Offline"}
+          </p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 rounded-md border border-border overflow-clip">
+          <ServerResponseItem
+            icon={Link2}
+            leftClassName="bg-muted/20 md:border-r border-b border-border"
+            rightClassName="border-b border-border"
+            title="Server Hostname"
+            content={
+              query.data?.java.hostname
+                ? query.data.java.hostname
+                : "Loading..."
+            }
+          />
+          <ServerResponseItem
+            icon={EthernetPort}
+            leftClassName="bg-muted/20 md:border-r border-b border-border"
+            rightClassName="border-b border-border"
+            title="Server Port"
+            content={
+              query.data?.java.port
+                ? query.data.java.port.toString()
+                : "Loading..."
+            }
+          />
+          <ServerResponseItem
+            icon={Milestone}
+            leftClassName="bg-muted/20 md:border-r border-b md:border-b-0 border-border"
+            rightClassName=""
+            title="Server Version"
+            content={
+              checkIfOnline(query.data?.java as MinecraftServerStatusResponse)
+                ? (query.data?.java as MinecraftServerOnlineStatus).version
+                : "Offline"
+            }
+          />
+        </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 rounded-md border border-border overflow-clip">
-        <ServerResponseItem
-          icon={Link2}
-          leftClassName="bg-muted/20 md:border-r border-b border-border"
-          rightClassName="border-b border-border"
-          title="Server Hostname"
-          content={query.data?.hostname ? query.data.hostname : "Loading..."}
-        />
-        <ServerResponseItem
-          icon={EthernetPort}
-          leftClassName="bg-muted/20 md:border-r border-b border-border"
-          rightClassName="border-b border-border"
-          title="Server Port"
-          content={query.data?.port ? query.data.port.toString() : "Loading..."}
-        />
-        <ServerResponseItem
-          icon={Milestone}
-          leftClassName="bg-muted/20 md:border-r border-b md:border-b-0 border-border"
-          rightClassName=""
-          title="Server Version"
-          content={
-            checkIfOnline(query.data as MinecraftServerStatusResponse)
-              ? (query.data as MinecraftServerOnlineStatus).version
-              : "Offline"
-          }
-        />
+      <div className="p-5 pt-3 border-t border-border">
+        <div className="flex items-center justify-between mb-3">
+          <h3 className="text-primary font-bold dark:font-semibold flex items-center gap-3">
+            <Activity className="size-5" />
+            <span>Server Status (Bedrock)</span>
+          </h3>
+          <p
+            className={cn(
+              "px-3 py-1 text-sm border rounded-full",
+              checkIfOnline(
+                query.data?.bedrock as MinecraftServerStatusResponse
+              )
+                ? "bg-green-800/20 border-green-800 dark:bg-green-500/20 dark:border-green-500 text-foreground"
+                : "bg-red-800/20 border-red-800 dark:bg-red-500/20 dark:border-red-500 text-foreground"
+            )}
+          >
+            {checkIfOnline(query.data?.bedrock as MinecraftServerStatusResponse)
+              ? "Online"
+              : "Offline"}
+          </p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 rounded-md border border-border overflow-clip">
+          <ServerResponseItem
+            icon={Link2}
+            leftClassName="bg-muted/20 md:border-r border-b border-border"
+            rightClassName="border-b border-border"
+            title="Server Hostname"
+            content={
+              query.data?.bedrock.hostname
+                ? query.data.bedrock.hostname
+                : "Loading..."
+            }
+          />
+          <ServerResponseItem
+            icon={EthernetPort}
+            leftClassName="bg-muted/20 md:border-r border-b border-border"
+            rightClassName="border-b border-border"
+            title="Server Port"
+            content={
+              query.data?.bedrock.port
+                ? query.data.bedrock.port.toString()
+                : "Loading..."
+            }
+          />
+          <ServerResponseItem
+            icon={Milestone}
+            leftClassName="bg-muted/20 md:border-r border-b md:border-b-0 border-border"
+            rightClassName=""
+            title="Server Version"
+            content={
+              checkIfOnline(
+                query.data?.bedrock as MinecraftServerStatusResponse
+              )
+                ? (query.data?.bedrock as MinecraftServerOnlineStatus).version
+                : "Offline"
+            }
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
