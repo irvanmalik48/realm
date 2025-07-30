f07721183c9923300ee681c44251708563382b33
Irvan Malik <irvanma@gnuweeb.org>
2025-07-31 02:14:31 +0700

    feat(comp/container): add classname
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,6 +1,16 @@
-export default function Container({ children }: { children: React.ReactNode }) {
+import { cn } from "@/lib/utils";
+
+export default function Container({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
   return (
-    <main className="w-full max-w-3xl mx-auto p-5 flex flex-col gap-5">
+    <main
+      className={cn`w-full max-w-3xl mx-auto p-5 flex flex-col gap-5 ${className}`}
+    >
       {children}
     </main>
   );
