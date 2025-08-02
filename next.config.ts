40ae1b61b83a426ee04d4ecfb2a99f093ebff52e
Irvan Malik <irvanma@gnuweeb.org>
2025-08-03 03:06:15 +0700

    fix(lastfm): cors adjustment
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,30 @@ const nextConfig: NextConfig = {
     serverSourceMaps: false,
   },
   productionBrowserSourceMaps: false,
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value:
+              process.env.NEXT_PUBLIC_ENVIRONMENT === "development"
+                ? "*"
+                : "irvanma.eu.org",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
