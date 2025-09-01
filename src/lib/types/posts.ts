82630d51f5a006455f7ae9332eb497d9166e372a
Irvan Malik <irvanma@gnuweeb.org>
2025-09-01 21:07:17 +0700

    feat(types/posts): add post related types
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

---
+++ src/lib/types/posts.ts
@@ -0,0 +1,23 @@
+export type Frontmatter = {
+  title: string;
+  createdAt: string;
+  updatedAt: string;
+  description: string;
+  tags: string[];
+};
+
+export type Slug = {
+  slug: string;
+};
+
+export type Post = Frontmatter & Slug;
+
+export type Scope = {
+  readingTime: string;
+};
+
+export type PostWithScope = Post & Scope;
+
+export interface PostCardAuxiliaries extends React.ComponentProps<"div"> {}
+
+export type PostCardProps = PostWithScope & PostCardAuxiliaries;
