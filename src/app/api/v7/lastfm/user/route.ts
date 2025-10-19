33d5cb6660b04fab648b8b0024071b25ed7823a1
Irvan Malik <irvanma@gnuweeb.org>
2025-10-19 19:38:11 +0700

    refactor(app/api/v7/lastfm/user): decouple
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- src/app/api/v7/lastfm/user/route.ts
+++ src/app/api/v7/lastfm/user/route.ts
@@ -1,4 +1,5 @@
 import { LastFMUserResponseBody } from "@/lib/types/lastfm";
+import { jsonResponse, errorResponse } from "@/lib/headers/index";
 
 export async function GET(request: Request): Promise<Response> {
   const token = process.env.LASTFM_API_KEY;
@@ -7,74 +8,27 @@ export async function GET(request: Request): Promise<Response> {
   const username = url.searchParams.get("username");
 
   if (!token) {
-    return new Response(
-      JSON.stringify({
-        error: "LastFM API key is not set.",
-        status: "error",
-      }),
-      {
-        status: 500,
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET",
-          "Access-Control-Allow-Headers": "Content-Type",
-        },
-      }
-    );
+    return errorResponse("LastFM API key is not set.", 500);
   }
 
   if (!username) {
-    return new Response(
-      JSON.stringify({
-        error: "Username is required.",
-        status: "error",
-      }),
-      {
-        status: 400,
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET",
-          "Access-Control-Allow-Headers": "Content-Type",
-        },
-      }
-    );
+    return errorResponse("Username is required.", 400);
   }
 
-  const endpoint = `https://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=${username}&api_key=${token}&format=json`;
+  const endpoint = `https://ws.audioscrobbler.com/2.0/?method=user.getinfo&user=${encodeURIComponent(
+    username,
+  )}&api_key=${encodeURIComponent(token)}&format=json`;
 
   const response = await fetch(endpoint, {
     next: {
       revalidate: 60 * 15, // 15 minutes
     },
   });
-  if (!response.ok) {
-    return new Response(
-      JSON.stringify({
-        error: "Failed to fetch LastFM user data",
-        status: "error",
-      }),
-      {
-        status: response.status,
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Methods": "GET",
-          "Access-Control-Allow-Headers": "Content-Type",
-        },
-      }
-    );
-  }
-  const body: LastFMUserResponseBody = await response.json();
 
-  return new Response(JSON.stringify(body), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET",
-      "Access-Control-Allow-Headers": "Content-Type",
-    },
-  });
+  if (!response.ok) {
+    return errorResponse("Failed to fetch LastFM user data", response.status);
+  }
+
+  const body: LastFMUserResponseBody = await response.json();
+  return jsonResponse(body, 200);
 }
