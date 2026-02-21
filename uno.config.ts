a98082775e23505178dc0d72d970f881c3ee68db
Irvan Malik <irvanma@gnuweeb.org>
2026-02-22 04:33:12 +0700

    feat: define custom classes
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- uno.config.ts
+++ uno.config.ts
@@ -15,6 +15,76 @@ export default defineConfig<Theme>({
       header: "Besley, serif",
       monospace: "Geist Mono, monospace",
       sans: 'Geist, ui-sans-serif, system-ui, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'
+    },
+    colors: {
+      border: "var(--border)",
+      input: "var(--input)",
+      ring: "var(--ring)",
+      background: "var(--background)",
+      foreground: "var(--foreground)",
+      primary: {
+        DEFAULT: "var(--primary)",
+        foreground: "var(--primary-foreground)"
+      },
+      secondary: {
+        DEFAULT: "var(--secondary)",
+        foreground: "var(--secondary-foreground)"
+      },
+      destructive: {
+        DEFAULT: "var(--destructive)",
+        foreground: "var(--destructive-foreground)"
+      },
+      muted: {
+        DEFAULT: "var(--muted)",
+        foreground: "var(--muted-foreground)"
+      },
+      accent: {
+        DEFAULT: "var(--accent)",
+        foreground: "var(--accent-foreground)"
+      },
+      popover: {
+        DEFAULT: "var(--popover)",
+        foreground: "var(--popover-foreground)"
+      },
+      card: {
+        DEFAULT: "var(--card)",
+        foreground: "var(--card-foreground)"
+      },
+      sidebar: {
+        DEFAULT: "var(--sidebar)",
+        foreground: "var(--sidebar-foreground)",
+        primary: "var(--sidebar-primary)",
+        "primary-foreground": "var(--sidebar-primary-foreground)",
+        accent: "var(--sidebar-accent)",
+        "accent-foreground": "var(--sidebar-accent-foreground)",
+        border: "var(--sidebar-border)",
+        ring: "var(--sidebar-ring)"
+      },
+      chart: {
+        1: "var(--chart-1)",
+        2: "var(--chart-2)",
+        3: "var(--chart-3)",
+        4: "var(--chart-4)",
+        5: "var(--chart-5)"
+      }
+    },
+    radius: {
+      lg: "var(--radius)",
+      md: "calc(var(--radius) - 2px)",
+      sm: "calc(var(--radius) - 4px)"
+    },
+    shadow: {
+      "2xs": "var(--shadow-2xs)",
+      xs: "var(--shadow-xs)",
+      sm: "var(--shadow-sm)",
+      DEFAULT: "var(--shadow)",
+      md: "var(--shadow-md)",
+      lg: "var(--shadow-lg)",
+      xl: "var(--shadow-xl)",
+      "2xl": "var(--shadow-2xl)"
+    },
+    tracking: {
+      normal: "var(--tracking-normal)"
     }
   }
 });
