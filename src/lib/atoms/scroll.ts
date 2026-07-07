93df2ba7492372b18f8ed4c8f80e4cb701d982e8
Irvan Malik <irvanma@gnuweeb.org>
2026-07-07 10:36:28 +0700

    feat(scroll): add custom scrollbar setting atom
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- src/lib/atoms/scroll.ts
+++ src/lib/atoms/scroll.ts
@@ -3,3 +3,4 @@ import { atomWithStorage } from "jotai/utils";
 export const smoothScrollAtom = atomWithStorage<boolean>("smoothScroll", true);
 export const scrollLerpAtom = atomWithStorage<number>("scrollLerp", 0.1);
 export const scrollDurationAtom = atomWithStorage<number>("scrollDuration", 1.2);
+export const customScrollbarEnabledAtom = atomWithStorage<boolean>("customScrollbarEnabled", true);
