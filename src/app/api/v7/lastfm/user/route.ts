5ab5480320ef2e049a92cf4f051c527d2492b251
Irvan Malik <irvanma@gnuweeb.org>
2026-08-04 09:14:25 +0700

    refactor(api): remove inline comment in lastfm user route
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- src/app/api/v7/lastfm/user/route.ts
+++ src/app/api/v7/lastfm/user/route.ts
@@ -21,7 +21,7 @@ export async function GET(request: Request): Promise<Response> {
 
   const response = await fetch(endpoint, {
     next: {
-      revalidate: 60 * 15, // 15 minutes
+      revalidate: 60 * 15,
     },
   });
 
