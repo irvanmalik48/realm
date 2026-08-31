1e819bbe6bef2d1a5888b82f1e7aed6fbe45328f
Irvan Malik <irvanma@gnuweeb.org>
2026-08-31 09:47:58 +0700

    feat(logos): add tech stack SVG icons
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

---
+++ src/components/logos/tech-logos.tsx
@@ -0,0 +1,154 @@
+import React from "react";
+
+export function NextjsIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm5.69 17.653l-6.726-8.627v8.627H9.286V6.347h1.678l6.726 8.627V6.347h1.678v11.306h-1.678z" />
+    </svg>
+  );
+}
+
+export function TypeScriptIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M1.125 0C.502 0 0 .502 0 1.125v21.75C0 23.498.502 24 1.125 24h21.75c.623 0 1.125-.502 1.125-1.125V1.125C24 .502 23.498 0 22.875 0H1.125zM14.618 10.37h-3.155v7.283H9.865V10.37H6.71V8.895h7.908v1.475zm6.848 5.762c0 1.706-1.393 2.766-3.717 2.766-2.227 0-3.666-.998-3.791-2.457h1.64c.094.695.844 1.148 2.062 1.148 1.172 0 1.945-.492 1.945-1.289 0-.82-.633-1.172-1.922-1.64l-.656-.235c-1.898-.68-2.812-1.547-2.812-3.047 0-1.781 1.476-2.789 3.515-2.789 2.086 0 3.445.961 3.586 2.414h-1.64c-.117-.656-.75-1.078-1.852-1.078-1.078 0-1.781.445-1.781 1.195 0 .727.539 1.055 1.758 1.477l.656.234c2.156.773 2.909 1.641 2.909 3.305z" />
+    </svg>
+  );
+}
+
+export function TailwindIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z" />
+    </svg>
+  );
+}
+
+export function ShadcnIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 256 256"
+      fill="none"
+      stroke="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <line
+        x1="208"
+        y1="128"
+        x2="128"
+        y2="208"
+        strokeWidth="24"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <line
+        x1="192"
+        y1="40"
+        x2="40"
+        y2="192"
+        strokeWidth="24"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export function MotionIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M4 0h16v8h-8zM4 8h8l8 8H4zM4 16h8v8z" />
+    </svg>
+  );
+}
+
+export function LenisIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M8 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-5" />
+      <path d="M18 4l3 3-9 9H9v-3l9-9z" />
+    </svg>
+  );
+}
+
+export function MdxIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M22.286 0H1.714C.768 0 0 .768 0 1.714v20.572C0 23.232.768 24 1.714 24h20.572C23.232 24 24 23.232 24 22.286V1.714C24 .768 23.232 0 22.286 0zM3.429 17.143V6.857h2.286l2.857 3.571 2.857-3.571h2.286v10.286h-2.286v-5.714l-2.857 3.571-2.857-3.571v5.714H3.429zm14.857 0l-3.429-4.571h2.286V6.857h2.286v5.715h2.286l-3.429 4.571z" />
+    </svg>
+  );
+}
+
+export function GoIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M1.811 10.231c-.047-.272-.03-.497.053-.664.126-.254.407-.406.84-.457.728-.087 1.83.218 2.89.897.408.26.791.564 1.137.9.09-.168.188-.337.297-.504.606-.937 1.393-1.696 2.29-2.203.882-.497 1.843-.728 2.825-.668 1.042.064 2.015.449 2.844 1.11.83.663 1.458 1.547 1.823 2.56.368 1.02.43 2.12.185 3.193-.244 1.07-.798 2.013-1.587 2.723-.79.71-1.745 1.166-2.77 1.314-.492.071-.992.062-1.487-.025-.503-.09-.982-.258-1.425-.499-.444-.241-.837-.55-1.168-.916-.14.17-.291.332-.452.484-.543.514-1.206.877-1.928 1.054-.722.176-1.488.156-2.215-.058-.455-.134-.73-.346-.826-.63-.064-.189-.044-.436.06-.729.13-.37.42-.787.842-1.218.423-.43.957-.872 1.554-1.284.07-.048.14-.095.213-.141-.124-.316-.215-.646-.272-.984-.066-.395-.062-.796.012-1.189.074-.393.219-.768.428-1.114-.143-.07-.282-.132-.416-.188-.707-.29-1.378-.426-1.954-.394-.575.031-1.029.215-1.319.539-.144.161-.225.358-.239.585zm13.785 1.127c-.02-.635-.205-1.246-.539-1.77-.333-.524-.803-.935-1.36-1.19-.558-.255-1.18-.328-1.8-.21-.62.117-1.187.423-1.637.884-.45.462-.733 1.047-.818 1.69-.085.643.027 1.292.325 1.874.298.583.766 1.057 1.353 1.371.587.315 1.258.435 1.938.347.67-.087 1.288-.4 1.764-.897.476-.496.757-1.144.802-1.832l-.028-.267z" />
+    </svg>
+  );
+}
+
+export function TanStackIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm0 3c3.866 0 7 3.134 7 7s-3.134 7-7 7-7-3.134-7-7 3.134-7 7-7zm-4.5 4.5v1.5h3v6h1.5v-6h3V9.5h-7.5z" />
+    </svg>
+  );
+}
+
+export function VercelIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path d="M24 22.525H0l12-21.05 12 21.05z" />
+    </svg>
+  );
+}
