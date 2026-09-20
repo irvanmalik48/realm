dfa174a3030e33efc9a67687333a567b95abe49a
Irvan Malik <irvanma@gnuweeb.org>
2026-09-20 22:41:47 +0700

    refactor(reactions): migrate framer-motion import to motion/react
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- src/components/blog-reactions.tsx
+++ src/components/blog-reactions.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState, useCallback } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion, AnimatePresence } from "motion/react";
 import {
   ThumbsUp,
   Heart,
