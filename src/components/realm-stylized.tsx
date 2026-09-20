ba6207f875a6b0358c70ed0d8ae3437c47e5fac2
Irvan Malik <irvanma@gnuweeb.org>
2026-09-20 22:42:19 +0700

    refactor(ui): migrate framer-motion import to motion/react in realm-stylized
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- src/components/realm-stylized.tsx
+++ src/components/realm-stylized.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { motion, useMotionValue, useSpring } from "framer-motion";
+import { motion, useMotionValue, useSpring } from "motion/react";
 
 export function RealmStylized(props: React.SVGProps<SVGSVGElement>) {
   const scale = useMotionValue(0);
