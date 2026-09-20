f8bef45a1afb0484554188d7500c084ba9bd3396
Irvan Malik <irvanma@gnuweeb.org>
2026-09-20 22:42:15 +0700

    refactor(toc): migrate framer-motion import to motion/react
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- src/components/table-of-contents.tsx
+++ src/components/table-of-contents.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { motion } from "framer-motion";
+import { motion } from "motion/react";
 import { cn } from "@/lib/utils";
 
 export interface Heading {
