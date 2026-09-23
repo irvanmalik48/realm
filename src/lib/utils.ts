8ec7c0f9a9e5277d479ecf141684d28704a43c74
Irvan Malik <irvanma@gnuweeb.org>
2026-09-23 10:16:30 +0700

    feat(utils): add safeJsonLd helper to sanitize json-ld scripts
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function safeJsonLd(data: unknown): string {
+  return JSON.stringify(data).replace(/</g, "\\u003c");
+}
