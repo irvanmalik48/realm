10d11f67ef41a17f82f7ca27e3331ebab97b80eb
Irvan Malik <irvanma@gnuweeb.org>
2026-09-23 14:04:04 +0700

    perf(theme-toggle): replace transition-all with transition-transform on icons
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- src/components/ui/theme-toggle-button.tsx
+++ src/components/ui/theme-toggle-button.tsx
@@ -21,8 +21,8 @@ export default function ThemeToggleButton() {
       className="w-9 p-0 h-9 relative group cursor-pointer transition-colors"
       name="Theme Toggle Button"
     >
-      <SunIcon className="size-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <MoonIcon className="absolute size-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+      <SunIcon className="size-[1.2rem] rotate-0 scale-100 transition-transform dark:-rotate-90 dark:scale-0" />
+      <MoonIcon className="absolute size-[1.2rem] rotate-90 scale-0 transition-transform dark:rotate-0 dark:scale-100" />
       <span className="sr-only">Theme Toggle </span>
     </Button>
   );
