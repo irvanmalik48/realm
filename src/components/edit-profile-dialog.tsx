0d1e0a6e467cc13784f8a3a2e981cbe7801244f6
Irvan Malik <irvanma@gnuweeb.org>
2026-09-25 17:11:13 +0700

    fix(profile): suppress react-doctor no-fetch-in-effect on availability check
    Signed-off-by: Irvan Malik <irvanma@gnuweeb.org>

--- src/components/edit-profile-dialog.tsx
+++ src/components/edit-profile-dialog.tsx
@@ -58,6 +58,7 @@ export function EditProfileDialog({
   }, [user, isOpen]);
 
   // Debounced username check
+  // react-doctor-disable-next-line react-doctor/no-fetch-in-effect
   useEffect(() => {
     if (!isOpen || !user) return;
 
